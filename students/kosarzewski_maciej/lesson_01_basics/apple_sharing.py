17fe12a2d40e5d18796c4925fab0474e5f0103cd
Maciej K <styroslaw@gmail.com>
2018-02-25 18:44:39 +0100

    apples sharing

---
+++ students/kosarzewski_maciej/lesson_01_basics/apple_sharing.py
@@ -0,0 +1,6 @@
+# Read the numbers like this:
+students = int(input())
+apples = int(input())
+# Print the result with print()
+print("Apples for 1 student: " + str(apples//students))
+print("Apples in basket: " + str(apples % students))
