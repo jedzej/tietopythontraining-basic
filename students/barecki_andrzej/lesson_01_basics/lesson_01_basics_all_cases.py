3cde92b0ca9b4ef306c1bd52fced2384f66744eb
AndrzejBarecki <abarecki2@interia.pl>
2018-03-01 18:13:28 +0100

    jedzej/lesson_01_basics: update file

--- students/barecki_andrzej/lesson_01_basics/lesson_01_basics_all_cases.py
+++ students/barecki_andrzej/lesson_01_basics/lesson_01_basics_all_cases.py
@@ -1,4 +1,6 @@
-# Lesson 1 (Input, print and numbers)
+# Lesson 1 (Input, print and numbers) and # Lesson 2 (Integer and float numbers)3
+import os
+import math
 
 print('Problem 1: Sum of three numbers')
 print('Set first number:')
@@ -7,112 +9,114 @@ print('Set second number:')
 b = int(input())
 print('Set third number:')
 c = int(input())
-print(a + b + c)
+result = a + b + c
+print(str(result) + os.linesep)
 
-print('\n\rProblem 2: Area of right-angled triangle')
+print('Problem 2: Area of right-angled triangle')
 print('Set length of the base of a right-angled triangle')
 b = int(input())
 print('Set length of the height of a right-angled triangle')
 h = int(input())
-print('The area of right-angled triangle is equal: ' + str((1/2)*b*h))
+print('The area of right-angled triangle is equal: ' + str((1 / 2) * b * h) + os.linesep)
 
-print('\n\rProblem 3: Hello, Harry!')
+print('Problem 3: Hello, Harry!')
 print('Set user name: ')
 name = input()
-print('Hello, ' + str(name) + '!')
+print('Hello, ' + str(name) + '!' + os.linesep)
 
-print('\n\rProblem 4: Apple sharing')
+print('Problem 4: Apple sharing')
 print('Set number of students:')
 N = int(input())
 print('Set number of apples:')
 K = int(input())
 print('Each student get ' + str(K // N) + ' apples')
-print('In basket remains ' + str(K % N) + ' apples')
+print('In basket remains ' + str(K % N) + ' apples' + os.linesep)
 
-print('\n\rProblem 5: Previous and next')
+print('Problem 5: Previous and next')
 print('Set an integer:')
 a = int(input())
 print('The next number for the number ' + str(a) + ' is ' + str(a + 1) + '.')
-print('The previous number for the number ' + str(a) + ' is ' + str(a - 1) + '.')
+print('The previous number for the number ' + str(a) + ' is ' + str(a - 1) + '.' + os.linesep)
 
-print('\n\rProblem 6: School desks')
-def compute_desks( nr ):
-    return ((nr + (nr % 2)))>>1;
-myClass =['a','b','c']
+print('Problem 6: School desks')
+
+
+def compute_desks(nr):
+    return (nr + (nr % 2)) >> 1
+
+
 a = 0
+i = 0
+while True:
+    a += compute_desks(int(input()))
+    i += 1
+    if i == 3:
+        break
+print(str(a) + os.linesep)
 
-for x in range(0, 3):
-    print('Set number of students in class ' + myClass[x] + ' :')
-    a += compute_desks( int(input()))
-print('So we need ' + str(a) + ' desks in total.')
-
-
-# Lesson 2 (Integer and float numbers)
-
-print('\n\rProblem 1: Last digit of integer')
+# Lesson 2 (Integer and float numbers)3
+print('Problem 1: Last digit of integer')
 print('Set number of integer:')
 a = int(input())
-print('Last digit of integer is: ' + str(a % 10))
+print('Last digit of integer is: ' + str(a % 10) + os.linesep)
 
-print('\n\rProblem 2: Tens digit.')
+print('Problem 2: Tens digit.')
 print('Set an integer:')
 a = int(input())
-print('Tens digit from integer is equal ' + str( (a%100)//10 ) )
+print('Tens digit from integer is equal ' + str((a % 100) // 10) + os.linesep)
 
-print('\n\rProblem 3: Sum of digits.')
+print('Problem 3: Sum of digits.')
 print('Set an integer:')
 a = int(input())
-first = a % 10
-second = (a%100)//10
-third = (a%1000)//100
-print('Tens digit from integer is equal ' + str( first + second + third ) + '.')
+first_digit = a % 10
+second_digit = (a % 100) // 10
+third_digit = (a % 1000) // 100
+print('Tens digit from integer is equal ' + str(first_digit + second_digit + third_digit) + '.' + os.linesep)
 
-print('\n\rProblem 4: Fractional part.')
+print('Problem 4: Fractional part.')
 print('Set an positive real number:')
 a = float(input())
-print('Fractional part from positive real number is equal '+ str(a%1))
+print('Fractional part from positive real number is equal ' + str(a % 1) + os.linesep)
 
-print('\n\rProblem 5: First digit after decimal point')
-from math import floor
+print('Problem 5: First digit after decimal point')
 print('Set an positive real number:')
 a = float(input())
-print('Fractional part from positive real number is equal '+ str(floor((a%1)*10)) + '.')
+print('Fractional part from positive real number is equal ' + str(int(math.floor((a % 1) * 10))) + '.' + os.linesep)
 
-print('\n\rProblem 6: Car route')
-from math import ceil
+print('Problem 6: Car route')
 print('Set car cover distance per day[km]:')
 N = int(input())
 print('Set total length of route[km]:')
 M = int(input())
-print('Number of days per route: ' + str(ceil(M/N)))
+print('Number of days per route: ' + str(math.ceil(M / N)) + os.linesep)
 
-print('\n\rProblem 7: Digital clock')
+print('Problem 7: Digital clock')
 print('Set the number of minutes that are passed since midnight')
 N = int(input())
 hours = int(N // 60)
 minutes = N - (hours * 60)
 print('On the digital clock is:')
-print(str(hours) + ' ' + str(minutes))
+print(str(hours) + ' ' + str(minutes) + os.linesep)
 
-print('\n\rProblem 8: Total cost')
+print('Problem 8: Total cost')
 print('Set dollar part cost per cupcake:')
-A = int(input())
+dollar_part_cost = int(input())
 print('Set cent part cost per cupcake:')
-B = int(input())
+cent_part_cost = int(input())
 print('Set nr of cupcake:')
-N = int(input())
-total_cents = (A * 100 + B ) * N
+cupcake_nr = int(input())
+total_cents = (dollar_part_cost * 100 + cent_part_cost) * cupcake_nr
 dollars = total_cents // 100
-cents = total_cents - ( dollars * 100 )
-print('Total cost: ' + str(dollars) + ' ' + str(cents))
+cents = total_cents - (dollars * 100)
+print('Total cost: ' + str(dollars) + ' ' + str(cents) + os.linesep)
 
-print('\n\rProblem 9: Clock face - 1')
+print('Problem 9: Clock face - 1')
 print('Set hours:')
 H = int(input())
 print('Set minutes:')
 M = int(input())
 print('Set seconds:')
 S = int(input())
-total_minutes = S/60 + M + H*60
+total_minutes = S / 60 + M + H * 60
 angle = total_minutes * 0.5
 print('The angle is equal %.3f' % angle)
