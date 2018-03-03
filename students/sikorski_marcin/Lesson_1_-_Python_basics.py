46b2196e020d0541afd0a3a30d6dd51c4edb0687
hakuzaijo <hakuzaijo@gmail.com>
2018-03-03 11:44:26 +0100

    Update after first review.

--- students/sikorski_marcin/Lesson_1_-_Python_basics.py
+++ students/sikorski_marcin/Lesson_1_-_Python_basics.py
@@ -1,128 +1,106 @@
 #1 Sum of three numbers
 
-a = int(input())
-b = int(input())
-c = int(input())
-print(a+b+c)
+firstNumber = int(input())
+secondNumber = int(input())
+thirdNumber = int(input())
+print(firstNumber+secondNumber+thirdNumber)
 
 #2 Area of right-angled triangle
-a = int(input())
-b = int(input())
-print(0.5*a*b)
+length = float(input())
+height = float(input())
+print(0.5*length*height)
 
 #3 Hello, Harry!
 
-a = input()
-print("Hello, "+ a + "!")
+name = input("What is your name?: ")
+print("Hello, "+ name + "!")
 
 #4 Apple sharing
-a = int(input())
-b = int(input())
-print(b//a)
-print(b%a)
+numberOfStudent = int(input("How many students? "))
+numberOfApples = int(input("How many apples? "))
+print(numberOfApples//numberOfStudent)
+print(numberOfApples%numberOfStudent)
 
 #5 Previous and next
-a = int(input())
-print('The next number for the number ' + str(a)  + " is " + str(a+1) + ".")
-print('The previous number for the number ' + str(a)  + " is " + str(a-1) + ".")
+numberToEnter = int(input("Type some number: "))
+print('The next number for the number ' + str(numberToEnter) + " is " + str(numberToEnter +1) + ".")
+print('The previous number for the number ' + str(numberToEnter) + " is " + str(numberToEnter -1) + ".")
 
 #6 School desks
-a = int(input())
-b = int(input())
-c = int(input())
+numberOfStudentsInFirstClass = int(input("How many students in first class? "))
+numberOfStudentsInSecondClass = int(input("How many students in second class? "))
+numberOfStudentsInThirdClass = int(input("How many students in third class? "))
 
-l = [a,b,c]
-sum = 0
-for x in l:
-    if x%2 != 0:
-        z = ((x+1)/2)
-        sum += z
-    else:
-        z = (x/2)
-        sum += z
-print(sum)
-
+print("We need " + str(numberOfStudentsInFirstClass//2 + numberOfStudentsInSecondClass//2 + numberOfStudentsInThirdClass//2 + numberOfStudentsInFirstClass% 2 + numberOfStudentsInSecondClass%2 + numberOfStudentsInThirdClass%2) + " desks in total.")
 #---------------------------
 #PART 2
 #---------------------------
 
 #1 Last digit of integer
-a = int(input())
-print(a%10)
+givenInteger = int(input())
+print(givenInteger%10)
 
 #2 Tens digit
-a = str(input())
-print(a[-2])
+givenIntegerNumber = int(input())
+print(givenIntegerNumber//10%10)
 
 #3 Sum of digits
-a = str(input())
-changeToString = str(a)
-sum = 0
-for eachNumber in changeToString:
-    sum += int(eachNumber)
-print(sum)
+threeDigetNumber = int(input("Enter a three digit number: "))
+firstDigit = threeDigetNumber//100
+secondDigit = threeDigetNumber//10%10
+thirdDigit  = threeDigetNumber%10
+print("Sum of digits equals: " + str(firstDigit+secondDigit+thirdDigit))
 
 #4 Fractional part
 import math
-a = float(input())
-frac, whole = math.modf(a)
-print(frac)
+positiveRealNumber = float(input("Enter a positive real number to print its fractional part: "))
+frac, whole = math.modf(positiveRealNumber)
+print("The fractional part " + str(frac))
 
 #5 First digit after decimal point
-import math
-a = float(input())
-fraction = math.modf(a)
-fractionFirstElement = str(fraction[0])
-print(fractionFirstElement[2])
+aPositiveRealNumber = float(input("Enter a positive real number to find the first digit after decimal point: "))
+print("The first digit is " + str(int(aPositiveRealNumber*10)%10))
 
 #6 Car route
 import math
-distanceCarCanTravel = int(input())
-distanceToTravel = int(input())
-print(math.ceil(distanceToTravel/distanceCarCanTravel))
+distanceCarCanTravel = int(input("Enter distance car can travel: "))
+distanceToTravel = int(input("Enter distance to travel: "))
+print("It will take " + str(math.ceil(distanceToTravel/distanceCarCanTravel))+ " days.")
 
 #7 Digital clock
-import datetime
-minutesPassedSinceMidnight =int(input())
-print(datetime.timedelta(minutes=minutesPassedSinceMidnight))
+numberOfMinutesPassedSinceMidnight = int(input("How many minutes passed since midnight? "))
+hours = numberOfMinutesPassedSinceMidnight//60
+minutes = numberOfMinutesPassedSinceMidnight%60
+print("On the clock there are " + str(hours) + " hours and " + str(minutes) + " minutes.")
 
 #8 Total cost
-dollars = int(input())
-cents = int(input())
-howManyCupcakes = int(input())
+dollars = int(input("Cupcake price in dollar is: "))
+cents = int(input("Cupcake price in cents is: "))
+howManyCupcakes = int(input("How many cupcakes you want to buy: "))
 price = dollars + (cents/100)
-howManyShouldIpay = price*howManyCupcakes
-
-dollarsInTotalToPay, centsInTotalToPay = divmod(howManyShouldIpay, 1)
-print(int(dollarsInTotalToPay), centsInTotalToPay*100)
+howManyShouldIpay = howManyCupcakes * (100 * dollars+cents)
+print("Total cost is " + str(howManyShouldIpay//100) + " dollars and " + str(howManyShouldIpay%100) + " cents.")
 
 #9 Clock face - 1
 while True:
     hours = int(input("Give me a hour between 0-12: "))
-    if hours > 12:
-        print("Number must be smaller than 12")
-        continue
-    elif hours < 0:
-        print("Number must be bigger than 0")
-        continue
+    if hours not in range(0, 13):
+        print("Number must be in range 0-12!")
     else:
-        minutes = int(input("Give me a minutes between 0-60: "))
-        if minutes > 60:
-            print("Number must be smaller than 60")
-            continue
-        elif minutes < 0:
-            print("Number must be bigger than 0")
-            continue
-        else:
-            seconds = int(input("Give me a seconds between 0-60"))
-            if seconds > 60:
-                print("Number must be smaller than 60")
-                continue
-            elif seconds < 0:
-                print("Number must be bigger than 0")
-                continue
-            else:
-                break
-print(hours, minutes, seconds)
+        break
 
-print(hours*30 + minutes*30/60 + seconds*30/3600)
+while True:
+    minutes = int(input("Give me a minutes between 0-60: "))
+    if minutes not in range(0, 61):
+        print("Number must be between 0-60!")
+    else:
+        break
+
+while True:
+    seconds = int(input("Give me a seconds between 0-60"))
+    if seconds not in range(0, 61):
+        print("Number must be between 0-60!")
+    else:
+        break
+
+print("The angle is " + str(hours * 30 + minutes * 30 / 60 + seconds * 30 / 3600))
