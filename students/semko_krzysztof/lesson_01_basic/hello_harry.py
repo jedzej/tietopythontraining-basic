a3c0379b991b94ed9f3068c8d83e72f7f2a190de
Krzysztof Semko <krzysztof.semko@tieto.com>
2018-03-04 15:32:57 +0100

    jedzej/lesson_01_basic
    Commit with lesson_01 tasks completed

---
+++ students/semko_krzysztof/lesson_01_basic/hello_harry.py
@@ -0,0 +1,12 @@
+# Problem «Hello, Harry!» (Easy)
+# Statement
+# Write a program that greets the user by printing the word "Hello", a comma,
+# the name of the user and an exclamation mark after it. See the examples below.
+#
+# Warning. Your program's output should strictly match the desired one, character by character.
+# There shouldn't be any space between the name and the exclamation mark.
+# You can use + operator to concatenate two strings. See the lesson for details.
+
+print('Who are you?')
+name = input()
+print('Hello, ' + name + '!')
---
+++ students/semko_krzysztof/lesson_01_basic/school_desks.py
@@ -0,0 +1,21 @@
+# Problem «School desks» (Hard)
+# Statement
+# A school decided to replace the desks in three classrooms. Each desk sits two students.
+# Given the number of students in each class, print the smallest possible number of desks that can be purchased.
+#
+# The program should read three integers: the number of students in each of the three classes, a, b and c respectively.
+#
+# In the first test there are three groups. The first group has 20 students and thus needs 10 desks.
+# The second group has 21 students, so they can get by with no fewer than 11 desks.
+# 11 desks is also enough for the third group of 22 students. So we need 32 desks in total.
+
+import math
+
+print("please input size of first group:")
+first = int(input())
+print("please input size of second group:")
+second = int(input())
+print("please input size of third group:")
+third = int(input())
+
+print('Number needed: ' + str(math.ceil(first / 2) + ceil(second / 2) + ceil(third / 2)))
