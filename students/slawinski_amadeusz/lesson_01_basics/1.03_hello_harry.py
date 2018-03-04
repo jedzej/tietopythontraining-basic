a9f18a2533d2a1396787330c63b94e9fb02ee264
Amadeusz Sławiński <amade@asmblr.net>
2018-03-04 21:07:09 +0100

    complete lesson 1 tasks

---
+++ students/slawinski_amadeusz/lesson_01_basics/1.03_hello_harry.py
@@ -0,0 +1,4 @@
+# Reads name from input and says Hello, name!
+
+name = input()
+print("Hello, " + name + "!")
---
+++ students/slawinski_amadeusz/lesson_01_basics/2.01_last_digit_of_integer.py
@@ -0,0 +1,4 @@
+# prints last number of digit (a)
+number = int(input())
+
+print(number % 10)
