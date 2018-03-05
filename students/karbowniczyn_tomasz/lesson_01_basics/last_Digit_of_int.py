3748f835c40fedbbe99faa7078d544e26707ec32
Karbowniczyn Tomasz <tomasz.karbowniczyn@tieto.com>
2018-03-05 07:28:36 +0100

    add lesson_01_basics files
    Signed-off-by: Karbowniczyn Tomasz <tomasz.karbowniczyn@tieto.com>

---
+++ students/karbowniczyn_tomasz/lesson_01_basics/last_Digit_of_int.py
@@ -0,0 +1,5 @@
+# last digit of integer
+# by tk
+
+a = int(input())
+print ((a) % 10)
---
+++ students/karbowniczyn_tomasz/lesson_01_basics/next_prev.py
@@ -0,0 +1,8 @@
+# by tk, next and prev number from number
+# prev and next
+
+liczba = int(input())
+
+#print ('you wrote: ' + str((liczba)))
+print ('The next number for the number '+ str(int((liczba))) +' is ' + str((liczba)+1))
+print ('The prev number for the number '+ str(int((liczba))) + ' is ' + str((liczba)-1))
