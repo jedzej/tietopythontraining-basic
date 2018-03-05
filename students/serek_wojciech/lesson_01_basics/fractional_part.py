2789af33814a43f3aa04ba84bf485522477d1193
Wojciech Serek <wojciech.serek@tieto.com>
2018-03-05 11:44:32 +0100

    Lesson 01 - solution of exercises

---
+++ students/serek_wojciech/lesson_01_basics/fractional_part.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Fractional part"""
+
+
+def main():
+    """Main function"""
+    value = float(input())
+
+    print(value - int(value))
+
+
+if __name__ == '__main__':
+    main()
