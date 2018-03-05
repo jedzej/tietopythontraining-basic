063aeb6ce8322e2753a2fceffdbcd1603a0ee231
Rafal Skowronski <rafal.skowronski@gmail.com>
2018-03-05 19:32:05 +0100

    Add implementation of: lesson 01, problem 02 - area of the right triangle

---
+++ students/skowronski_rafal/lesson_01_basics/lesson_01/problem_02.py
@@ -0,0 +1,12 @@
+# Calculates area of the right triangle
+
+def print_area_of_right_triangle():
+    base = float(input('Enter base: '))
+    height = float(input('Enter height: '))
+    
+    area = 0.5 * base * height
+
+    print('Area of the triangle: ', area)
+
+if __name__ == '__main__':
+    print_area_of_right_triangle()
