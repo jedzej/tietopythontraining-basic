348b43ce9e33483a9feacfb2d3beda92682591ea
vSupas <36338390+vSupas@users.noreply.github.com>
2018-03-06 12:56:57 +0100

    Update Previous_and_next.py

--- students/slupek_bartosz/lesson_01_python_basic/Previous_and_next.py
+++ students/slupek_bartosz/lesson_01_python_basic/Previous_and_next.py
@@ -1,3 +1,3 @@
 a = int(input())
-print('The next number for the a ' + str(a) + ' is ' + str(a +1) +'.')
-print('The previous number for the a ' + str(a) + ' is ' + str(a -1) +'.')
+print('The next number for the a ' + str(a) + ' is ' + str(a + 1) +'.')
+print('The previous number for the a ' + str(a) + ' is ' + str(a - 1) +'.')
