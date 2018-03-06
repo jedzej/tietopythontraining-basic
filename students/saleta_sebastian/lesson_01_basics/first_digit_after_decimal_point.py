631e317f9122aae583fb3d86c030f9d7ac21b944
saletseb <sebastian.saleta@tieto.com>
2018-03-06 17:46:35 +0100

    Format print output

--- students/saleta_sebastian/lesson_01_basics/first_digit_after_decimal_point.py
+++ students/saleta_sebastian/lesson_01_basics/first_digit_after_decimal_point.py
@@ -1,6 +1,6 @@
 def get_first_digit_of_the_decimal_point(num):
     first_decimal_digit = int(num * 10) % 10
-    print(first_decimal_digit)
+    print('first decimal digit is {}'.format(first_decimal_digit))
 
 
 if __name__ == '__main__':
