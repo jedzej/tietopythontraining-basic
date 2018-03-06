e9a1ba0e2c8948a8bb10a8e1eda7c825d6882f43
Michal Piechowski <michal.a.piechowski@tieto.com>
2018-03-06 17:04:59 +0100

    Add lesson_03 exercises

---
+++ students/piechowski.michal/lesson_03_functions/collatz_sequence.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    else:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+print("Give number for collatz function:")
+try:
+    number = int(input())
+except ValueError:
+    print("You must input an integer!")
+    quit()
+
+if number < 1:
+    print("Wrong number, it needs to be >= 1")
+    quit()
+
+while True:
+    number = collatz(number)
+    if number == 1:
+        break
