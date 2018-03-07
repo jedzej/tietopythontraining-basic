58f046710c5e81dc7385ad6331276f8892d4c14a
Julita Bedkowska <julitabedkowska@gmail.com>
2018-03-07 21:22:48 +0100

    Snakify lesson 1

---
+++ students/bedkowska_julita/lesson_01_basics/apple_sharing.py
@@ -0,0 +1,5 @@
+n = int(input("input number of students: "))
+k = int(input("input number of apples: "))
+
+print(k // n)
+print(k % n)
---
+++ students/bedkowska_julita/lesson_01_basics/previous_and_next.py
@@ -0,0 +1,4 @@
+number = int(input("Input number: "))
+
+print("The next number for the number " + str(number) + " is " + str(number+1)+".")
+print("The previous number for the number " + str(number) + " is " + str(number-1)+".")
