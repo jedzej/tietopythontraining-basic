564d64e88c0673641bd3340be5164e733f11de5c
Michal Piechowski <michal.a.piechowski@tieto.com>
2018-03-09 20:26:20 +0100

    Add lesson_04 exercises

---
+++ students/piechowski.michal/lesson_04_unit_testing/collatz_tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_name + "/lesson_03_functions")
+
+import unittest
+from collatz_sequence import collatz
+
+
+class CollatzTest(unittest.TestCase):
+
+    def test_collatz_raises_type_error_with_invalid_input(self):
+        self.assertRaises(TypeError, collatz, 'aoeu')
+
+    def test_collatz_returns_correct_value(self):
+        self.assertEqual(collatz(8), 4)
+        self.assertEqual(collatz(5), 16)
+
+    def test_collatz_raises_value_error_with_negative_input(self):
+        self.assertRaises(ValueError, collatz, -1)
+
+
+if __name__== "__main__":
+    unittest.main()
---
+++ students/piechowski.michal/lesson_04_unit_testing/distance_tests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Run with:
+# pytest distance_tests.py
+
+import pytest
+import os
+import sys
+parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_name + "/lesson_03_functions")
+
+from the_length_of_the_segment import distance
+
+
+def test_distance_from_the_same_point_to_the_same_point_is_zero():
+    assert distance(1, 2, 1, 2) == 0
+    assert distance(0, 0, 0, 0) == 0
+
+
+def test_coordinates_sign_does_not_influence_distance():
+    assert distance(-1, -2, -3, -4) == distance(1, 2, 3, 4)
+
+
+def test_distance_of_points_on_the_same_line_is_equal_to_line_length():
+    assert distance(0, 0, 0, 7) == 7
+    assert distance(0, 0, 7, 0) == 7
+
+
+def test_that_order_of_points_does_not_matter():
+    assert distance(5, 6, 7, 8) == distance(7, 8, 5, 6)
+
+
+def test_distance_raises_type_error_with_invalid_input():
+    with pytest.raises(TypeError) as exception_info:
+            distance('aoeu', 0, 0, 0)
+
+    with pytest.raises(TypeError) as exception_info:
+            distance(None, 0, 0, 0)
