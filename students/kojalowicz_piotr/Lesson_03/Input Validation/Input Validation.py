6df42a5f52d95c51415a1a2892a81383748612f7
kojalowicz <36206988+kojalowicz@users.noreply.github.com>
2018-03-09 18:31:46 +0100

    Poczatek_lekcji_trzeciej

---
+++ students/kojalowicz_piotr/Lesson_03/Input Validation/Input Validation.py
@@ -0,0 +1,18 @@
+def collatz(number):
+    if number % 2 == 0:
+        return number // 2
+    else:
+        return 3 * number + 1
+
+
+def collatz_loop():
+    number = int(input())
+    while number != 1:
+        number = collatz(number)
+        print(number)
+
+
+try:
+    collatz_loop()
+except ValueError:
+    print('must enter an integer')
