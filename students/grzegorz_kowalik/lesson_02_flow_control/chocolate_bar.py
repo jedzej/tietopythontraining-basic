8a3055117edb60df2b0507c281487f859fd0303b
Grzegorz Kowalik <kowalik.grzegorz@outlook.com>
2018-03-10 15:05:14 +0100

    Fix stickler warnings

--- students/grzegorz_kowalik/lesson_02_flow_control/chocolate_bar.py
+++ students/grzegorz_kowalik/lesson_02_flow_control/chocolate_bar.py
@@ -2,9 +2,7 @@ rows = int(input())
 cols = int(input())
 k = int(input())
 
-if k > rows * cols:
-    print("NO")
-elif k % cols == 0 or k % rows == 0:
+if k < rows * cols and (k % cols == 0 or k % rows == 0):
     print("YES")
 else:
     print("NO")
