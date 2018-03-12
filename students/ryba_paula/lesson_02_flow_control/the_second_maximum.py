586d5bdf6f5eb7bc90c2a4138391dac3c1aa4d0e
Paula Ryba <paula.ryba@sonymobile.com>
2018-03-12 17:06:42 +0100

    Fix the_second_maximum.py from lesson_02

--- students/ryba_paula/lesson_02_flow_control/the_second_maximum.py
+++ students/ryba_paula/lesson_02_flow_control/the_second_maximum.py
@@ -1,13 +1,13 @@
-number = int(input())
-
-largest = number
+largest = 0
 second_largest = 0
 
-while number != 0:
+while True:
     number = int(input())
+    if number == 0:
+        break
     if number > largest:
         second_largest, largest = largest, number
-    elif number > second_largest:
+    elif number > second_largest and number < largest:
         second_largest = number
 
 print(second_largest)
