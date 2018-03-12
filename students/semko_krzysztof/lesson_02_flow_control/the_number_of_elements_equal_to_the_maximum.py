ccb5a815a638a57f63eba7c31e289883f3735ca4
semkokrz <36616225+semkokrz@users.noreply.github.com>
2018-03-12 13:23:29 +0100

    Update the_number_of_elements_equal_to_the_maximum.py

--- students/semko_krzysztof/lesson_02_flow_control/the_number_of_elements_equal_to_the_maximum.py
+++ students/semko_krzysztof/lesson_02_flow_control/the_number_of_elements_equal_to_the_maximum.py
@@ -4,16 +4,14 @@ Determine how many elements of this sequence are equal to
 its largest element.
 """
 
-print("Please input integers in a sequence. to finish, type '0'.")
-loop_number = 1
+print("Please input integers in a sequence. To finish, type '0'.")
+
 max_value = 0
 max_count = 0
-while loop_number > 0:
+while True:
     value = int(input())
-    if loop_number > 2:
-        if value == 0:
-            loop_number = 0
-            break
+    if value == 0:
+        break
 
     if value == max_value:
         max_count += 1
@@ -21,7 +19,6 @@ while loop_number > 0:
     if value > max_value:
         max_value = value
         max_count = 1
-    loop_number += 1
 
 print("Number of integers with max value (" + str(max_value) + ") = "
       + str(max_count))
