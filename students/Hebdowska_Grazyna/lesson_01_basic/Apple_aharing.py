a361ac53413a7004acfaae0bdef0006b79057d0c
Grazyna Hebdowska <grazyna.hebdowska@gmail.com>
2018-03-12 15:29:20 +0100

    Lesson 1
        Lesson 1
    Hebdowska_Grazyna/lesson_00_the_setup/importablqe_module.py

---
+++ students/Hebdowska_Grazyna/lesson_01_basic/Apple_aharing.py
@@ -0,0 +1,8 @@
+n_students = int(input("Give students number: "))
+k_appels = int(input("Give apple number: "))
+
+student_appels = int(k_appels / n_students)
+basket_appels = k_appels - (n_students * student_appels)
+
+print (student_appels)
+print(basket_appels)
