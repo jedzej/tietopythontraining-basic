aced468a7dbed559d4824d01a8bcad3f72632f86
Paula Ryba <paula.ryba@sonymobile.com>
2018-03-12 11:05:47 +0100

    Fix tasks from lesson_02_flow_control

--- students/ryba_paula/lesson_02_flow_control/factorial.py
+++ students/ryba_paula/lesson_02_flow_control/factorial.py
@@ -2,7 +2,7 @@ n = int(input())
 
 factorial = 1
 
-for i in range(1, n + 1):
-    factorial = factorial * i
+for i in range(2, n + 1):
+    factorial *= i
 
 print(factorial)
--- students/ryba_paula/lesson_02_flow_control/lost_card.py
+++ students/ryba_paula/lesson_02_flow_control/lost_card.py
@@ -1,12 +1,11 @@
 cards = int(input())
 
 sum_of_cards = 0
-remaining_cards = 0
 
 for i in range(1, cards + 1):
     sum_of_cards += i
 
-for i in range(1, cards):
-    remaining_cards += int(input())
+for i in range(cards - 1):
+    sum_of_cards -= int(input())
     
-print(sum_of_cards - remaining_cards)
+print(sum_of_cards)
