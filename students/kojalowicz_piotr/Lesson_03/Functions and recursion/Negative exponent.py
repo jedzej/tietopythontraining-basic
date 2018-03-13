78a1e9e42f7f2a7f083059937debffd1ea88a635
kojalowicz <36206988+kojalowicz@users.noreply.github.com>
2018-03-13 11:47:41 +0100

    Add lesson_3

---
+++ students/kojalowicz_piotr/Lesson_03/Functions and recursion/Negative exponent.py
@@ -0,0 +1,11 @@
+def power(base, index):
+    return base**index
+
+
+correctInPut = None
+while correctInPut is None:
+    try:
+        print(power(float(input()), int(input())))
+        correctInPut = 1
+    except ValueError:
+        print('The first number must be real and the second integer')
