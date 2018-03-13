8b63730b8a4a3f28329200d8493da84b66b0be14
Glogowska Joanna <joanna.glogowska@tieto.com>
2018-03-13 14:45:16 +0100

    Complete lesson 01. Fix?

--- students/Glogowska_Joanna/lesson_01_basics/lesson_01.py
+++ students/Glogowska_Joanna/lesson_01_basics/lesson_01.py
@@ -1,5 +1,8 @@
+import math
+
 # Lesson 1.Input, print and numbers
 # Sum of three numbers
+print('Input three numbers in different rows')
 first = int(input())
 second = int(input())
 third = int(input())
@@ -7,36 +10,48 @@ sum = first + second + third
 print(sum)
 
 # Area of right-angled triangle
+print('Input the length of the triangle')
 base = int(input())
+print('Input the height of the triangle')
 height = int(input())
 area = (base * height) / 2
 print(area)
 
 # Hello, Harry!
+print('What is your name?')
 print('Hello, ' + str(input()) + '!')
 
 # Apple sharing
+print('Input number of students')
 students = int(input())
+print('Input number of apples')
 apples = int(input())
-print(apples // students)
-print(apples % students)
+print('Each student will get ' + str(apples // students) + ' apples')
+print(str(apples % students) + ' apples will remain in the basket')
 
 # Previous and next
 number = int(input())
-print('The next number for the number ' + str(number) + ' is ' + str(number + 1) + '.')
-print('The previous number for the number ' + str(number) + ' is ' + str(number - 1) + '.')
+print('The next number for the number ' + str(number) + ' is ' +
+      str(number + 1) + '.')
+print('The previous number for the number ' + str(number) + ' is ' +
+      str(number - 1) + '.')
 
 # School desks
+print('There are 3 classes - how many students is in each of them?\n"'
+      'Input your answers in separate rows')
 class1 = int(input())
 class2 = int(input())
 class3 = int(input())
-desks = class1 // 2 + class2 // 2 + class3 // 2 + class1 % 2 + class2 % 2 + class3 % 2
-print(desks)
+desks = (
+    class1 // 2 + class2 // 2 + class3 // 2 +
+    class1 % 2 + class2 % 2 + class3 % 2
+)
+print(str(desks) + ' needs to be purchased')
 
 # Lesson 2.Integer and float numbers
 # Last digit of integer
 intnum = int(input())
-print(intnum%10)
+print(intnum % 10)
 
 # Tens digit
 tensdig = int(input())
@@ -52,17 +67,36 @@ print(fract - int(fract))
 
 # First digit after decimal point
 rand = float(input())
-firstdig = ((rand*10)//1)%10
+firstdig = ((rand * 10) // 1) % 10
 print(int(firstdig))
 
 # Car route
-
+print('What distance in km the car covers per day?')
+kmperday = int(input())
+print('What is the length of the route?')
+routelength = int(input())
+print(math.ceil(routelength / kmperday))
 
 # Digital clock
-
+print('Number of minutes that passed since midnight')
+minsmidn = int(input())
+print(str(minsmidn // 60) + ':' + str(minsmidn % 60))
 
 # Total cost
-
+dollars = int(input())
+cents = int(input())
+numcup = int(input())
+print(dollars * numcup + (cents * numcup) // 100, (cents * numcup) % 100)
 
 # Clock face - 1
-
+print('How many hours have passed since minight?')
+hours = int(input())
+print('How many minutes have passed since minight?')
+mins = int(input())
+print('How many seconds have passed since minight?')
+secs = int(input())
+anghr = 360 / 12
+angmin = anghr * 1 / 60
+angsec = angmin / 60
+angle = (anghr * hours) + (angmin * mins) + (angsec * secs)
+print(angle)
