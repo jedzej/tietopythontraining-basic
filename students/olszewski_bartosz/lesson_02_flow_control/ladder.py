631746bf71bad53830d8b42a41ce59c6b54d092a
olszebar <bartosz.olszewski@tieto.com>
2018-03-13 10:54:06 +0100

    Update ladder.py

--- students/olszewski_bartosz/lesson_02_flow_control/ladder.py
+++ students/olszewski_bartosz/lesson_02_flow_control/ladder.py
@@ -1,5 +1,5 @@
 ladder = int(input())
-for j in range(1, ladder+1):
-    for i in range(1, j+1):
+for j in range(1, ladder + 1):
+    for i in range(1, j + 1):
         print(i, end='')
     print(sep='\n')
