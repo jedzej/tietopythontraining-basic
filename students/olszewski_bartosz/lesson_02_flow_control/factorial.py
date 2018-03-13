3f25bab6eaa9078185f7e0f02a93423a7ef7c58f
olszebar <bartosz.olszewski@tieto.com>
2018-03-13 10:45:26 +0100

    Lesson 02 Flow control done

---
+++ students/olszewski_bartosz/lesson_02_flow_control/factorial.py
@@ -0,0 +1,5 @@
+result = 1
+factorial = int(input())
+for i in range(1, factorial + 1):
+    result *= i
+print(result)
---
+++ students/olszewski_bartosz/lesson_02_flow_control/the_number_of_zeros.py
@@ -0,0 +1,7 @@
+number_of_zeros = 0
+inputs = int(input())
+for i in range (1, inputs + 1):
+    item = int(input())
+    if item == 0:
+        number_of_zeros += 1
+print(number_of_zeros)
