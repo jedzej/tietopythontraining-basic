4b8da1b863d2f149efcafe17a16c98a67bcdb096
vSupas <36338390+vSupas@users.noreply.github.com>
2018-03-13 09:39:59 +0100

    Update Ladder.py

--- students/slupek_bartosz/lesson_02_flow_control/Ladder.py
+++ students/slupek_bartosz/lesson_02_flow_control/Ladder.py
@@ -1,7 +1,5 @@
 height = int(input())
 for row in range(height):
-    word = ""
     for number in range(row + 1):
-        print(number + 1, end='')
-    print("")
-    
+        print number + 1, end=''
+    print ""
