7238ae65093e888f5172a62f5eaf1d10126cb159
myszkpaw <37329235+myszkpaw@users.noreply.github.com>
2018-03-13 19:40:49 +0100

    Test commit:

---
+++ students/myszko_pawel/lesson_00_the_setup/hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+
+"""This is an example module to teach you about git and PyCharm use.
+
+
+
+Python supports many ways to write programs. You can start with a very simple
+
+imperative code, like this script.
+
+"""
+
+
+
+print("Hello world!")
