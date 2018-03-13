a2614eb98f964a089379f78fa9cc16cbbae1550d
Stachowska Agata <agata.stachowska@tieto.com>
2018-03-13 10:53:51 +0100

    fixed stickled errors

--- students/stachowska_agata/lesson_02_flow_control/knight_move.py
+++ students/stachowska_agata/lesson_02_flow_control/knight_move.py
@@ -3,7 +3,8 @@ y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 
-if (abs(x2 - x1) == 2 and abs(y2 - y1) == 1) or (abs(y2 - y1) == 2 and abs(x2 - x1) == 1):
+if (abs(x2 - x1) == 2 and abs(y2 - y1) == 1) \
+        or (abs(y2 - y1) == 2 and abs(x2 - x1) == 1):
     print("YES")
 else:
     print("NO")
