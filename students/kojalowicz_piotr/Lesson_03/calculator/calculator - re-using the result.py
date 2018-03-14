1316c2a6f89a85ccfd0c44a585e662f51b3590ab
kojalowicz <36206988+kojalowicz@users.noreply.github.com>
2018-03-14 17:57:35 +0100

    add new line

--- students/kojalowicz_piotr/Lesson_03/calculator/calculator - re-using the result.py
+++ students/kojalowicz_piotr/Lesson_03/calculator/calculator - re-using the result.py
@@ -74,7 +74,8 @@ def calculations(options, new_result=None):
     elif options == "m":
         operands = ([new_result] + input_operands("MULTIPLY", ["multiplier"])
                     if new_result is not None
-                    else input_operands("MULTIPLY", ["multiplicand", "multiplier"]))
+                    else input_operands("MULTIPLY",
+                                        ["multiplicand", "multiplier"]))
         value = multiply(operands[0], operands[1])
         result(value)
         return value
