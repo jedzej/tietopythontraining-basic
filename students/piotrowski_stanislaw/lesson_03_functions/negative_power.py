880ef83c84a861b6e08d38eab5f1c9ed55263eae
Stanislaw Piotrowski <stanislaw.piotrowski@gmail.com>
2018-03-14 09:00:34 +0100

    Lesson 03 - practice projects 1,2,3

---
+++ students/piotrowski_stanislaw/lesson_03_functions/negative_power.py
@@ -0,0 +1,11 @@
+# https://snakify.org/lessons/functions/problems/negative_power/
+# piotrsta
+
+a = float(input())
+n = float(input())
+
+def power(a, n):
+    result = a ** n
+    return result
+
+print(power(a, n))
