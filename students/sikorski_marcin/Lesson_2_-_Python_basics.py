bbcd536d123aa42f35f2e4e95c24ccee79ad868f
hakuzaijo <hakuzaijo@gmail.com>
2018-03-14 20:00:21 +0100

    Lesson 2 after reviews

--- students/sikorski_marcin/Lesson_2_-_Python_basics.py
+++ students/sikorski_marcin/Lesson_2_-_Python_basics.py
@@ -1,37 +1,30 @@
 #Lesson_3
 #Leap Year
-year = int(input("Enter a year to check: "))
+year = int(input("Enter year to check: "))
 
-while year > 0:
-    if (year % 400 == 0):
-        print("LEAP")
-        break
-    else:
-        if (year%4 == 0) and (year%100 != 0):
-            print("LEAP")
-            break
-        else:
-            print("COMMON")
-            break
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+    print('LEAP')
+else:
+    print('COMMON')
 
 #Chocolate bar
-firstValueOfPortion = int(input("Enter first value: "))
-secondValueOFPortion = int(input("Enter second value: "))
-numberOfSquares = int(input("Enter number of squares: "))
+first_value_of_portion_ortion = int(input("Enter first value: "))
+second_value_of_portion = int(input("Enter second value: "))
+number_of_squares = int(input("Enter number of squares: "))
 
-if (numberOfSquares%firstValueOfPortion == 0 and numberOfSquares/firstValueOfPortion < secondValueOFPortion) or (numberOfSquares%secondValueOFPortion == 0 and numberOfSquares/secondValueOFPortion < firstValueOfPortion):
+if (number_of_squares %first_value_of_portion_ortion == 0 and number_of_squares /first_value_of_portion_ortion < second_value_of_portion) or (number_of_squares %second_value_of_portion== 0 and number_of_squares /second_value_of_portion < first_value_of_portion_ortion):
     print("YES - we can divide choclate!")
 else:
     print("NO - we can't divide chocolate")
 
 #Knight move
-firstColumn = int(input("Enter first column: "))
-firstRow = int(input("Enter first row: "))
-secondColumn = int(input("Enter second column: "))
-secondRow = int(input("Enter second row: "))
+first_column = int(input("Enter first column: "))
+first_row = int(input("Enter first row: "))
+second_column = int(input("Enter second column: "))
+second_row = int(input("Enter second row: "))
 
-row = abs(firstRow-secondRow)
-column = abs(firstColumn-secondColumn)
+row = abs(first_row-second_row)
+column = abs(first_column -second_column)
 
 if row == 2 and column == 1 or row == 2 and column == 1:
     print("YES - we can move there!")
@@ -39,28 +32,28 @@ else:
     print("NO - we can't move there!")
 
 #Queen move
-firstColumn = int(input("Enter first column: "))
-firstRow = int(input("Enter first row: "))
-secondColumn = int(input("Enter second column: "))
-secondRow = int(input("Enter second row: "))
+first_column = int(input("Enter first column: "))
+first_row = int(input("Enter first row: "))
+second_column = int(input("Enter second column: "))
+second_row = int(input("Enter second row: "))
 
-row = abs(firstRow-secondRow)
-column = abs(firstColumn-secondColumn)
+row = abs(first_row-second_row)
+column = abs(first_column -second_column)
 
 
-if row == column or firstRow == secondRow or firstColumn== secondColumn:
+if row == column or first_row == second_row or first_column == second_column:
     print("YES - we can move the queen there!")
 else:
     print("NO - we can't move queen there")
 
 #Bishop Moves
-firstColumn = int(input("Enter first column: "))
-firstRow = int(input("Enter first row: "))
-secondColumn = int(input("Enter second column: "))
-secondRow = int(input("Enter second row: "))
+first_column = int(input("Enter first column: "))
+first_row = int(input("Enter first row: "))
+second_column = int(input("Enter second column: "))
+second_row = int(input("Enter second row: "))
 
-row = abs(firstRow-secondRow)
-column = abs(firstColumn-secondColumn)
+row = abs(first_row -second_row)
+column = abs(first_column-second_column)
 
 if row == column:
     print("YES - we can move bishop there!")
@@ -69,53 +62,53 @@ else:
 
 #Lesson 4
 #Lost cards
-numberOfCards = int(input("Enter number of cards: "))
-sumOfNumberOfCards = (((1 + numberOfCards)/2) * (numberOfCards))
+number_of_cards = int(input("Enter number of cards: "))
+sum_of_number_of_cards = (((1 + number_of_cards)/2) * number_of_cards)
 
 sum = 0
-while numberOfCards != 1:
+while number_of_cards != 1:
     number = int(input("Type NOT MISSING numbers: "))
-    numberOfCards -= 1
+    number_of_cards -= 1
     sum += number
 
-print("Number of lost cards: " +str(sumOfNumberOfCards-sum))
+print("Number of lost cards: " +str(sum_of_number_of_cards-sum))
 
 #Ladder
-numberOfSteps = int(input("Enter number of steps: "))
+number_of_steps = int(input("Enter number of steps: "))
 
-for x in range(1, numberOfSteps + 1):
+for x in range(1, number_of_steps + 1):
     for y in range(1, x + 1):
         print(y, sep='', end='')
     print()
 
 #Adding factorials
-numberToCheck = int(input("Give me a number to do facorial" ))
+number_to_check = int(input("Give me a number to do facorial" ))
 
-whichFactorial = 1
+which_factoral = 1
 sum=0
 
-if numberToCheck != 0:
-    for i in range (1, numberToCheck+1):
-        whichFactorial *= i
-        sum += whichFactorial
+if number_to_check != 0:
+    for i in range (1, number_to_check+1):
+        which_factoral *= i
+        sum += which_factoral
     print(sum)
 else:
     print(1)
 
-#Tje number of zeroes
-numberOfZeroes = 0
+#The number of zeroes
+number_of_zeroes = 0
 
 for i in range(int(input("How many numbers you want to type? "))):
     if int(input("Type the number "+ str(i+1) + "  ")) == 0:
-        numberOfZeroes += 1
-print("You have " + str(numberOfZeroes) + " zeroes.")
+        number_of_zeroes += 1
+print("You have " + str(number_of_zeroes) + " zeroes.")
 
 #Factorial
 
 factorial = int(input("Type the number: "))
 starter = 1
 
-for i in range(1, factorial+1):
+for i in range(2, factorial+1):
     starter *= i
 print(starter)
 
@@ -123,7 +116,7 @@ print(starter)
 #The number of elements equal to the maximum
 
 maximum = 0
-howManyMaximumNumbers = 0
+how_many_maximum_numbers = 0
 counter = 1
 
 print("Type the numbers. Stop with 0.")
@@ -131,7 +124,7 @@ print("Type the numbers. Stop with 0.")
 while counter != 0:
     counter = int(input())
     if counter > maximum:
-        maximum, howManyMaximumNumbers = counter, 1
+        maximum, how_many_maximum_numbers = counter, 1
     elif counter == maximum:
-        howManyMaximumNumbers += 1
-print("Maxium number appears: " + str(howManyMaximumNumbers))
+        how_many_maximum_numbers += 1
+print("Maxium number appears: " + str(how_many_maximum_numbers))
