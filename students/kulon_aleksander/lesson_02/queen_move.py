8750f8c5b575564ccc35747adbc8d29c8c626448
Aleksander Kulon <olo3@poczta.onet.pl>
2018-03-15 19:18:35 +0100

    i dont get it...
    Signed-off-by: Aleksander Kulon <olo3@poczta.onet.pl>

--- students/kulon_aleksander/lesson_02/queen_move.py
+++ students/kulon_aleksander/lesson_02/queen_move.py
@@ -7,9 +7,10 @@ cell2_y = int(input())
 distance_x = abs(cell1_x - cell2_x)
 distance_y = abs(cell1_y - cell2_y)
 
-if ((cell1_x - distance_x == cell2_x) or (cell1_x + distance_x == cell2_x)) \
-   and ((cell1_y - distance_x == cell2_y) \
-        or (cell1_y + distance_x == cell2_y)):
+if ((cell1_x - distance_x == cell2_x) or
+        (cell1_x + distance_x == cell2_x)) and \
+    ((cell1_y - distance_x == cell2_y) or
+        (cell1_y + distance_x == cell2_y)):
     print('YES')
 elif distance_y == 0:
     print('YES')
