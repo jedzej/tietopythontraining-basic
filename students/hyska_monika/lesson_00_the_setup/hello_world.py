6f8126f02267b28783f5d4b88d299fbb015abfa1
zelazmon <hyska.monika@gmail.com>
2018-03-15 13:33:49 +0100

    hello word in function

--- students/hyska_monika/lesson_00_the_setup/hello_world.py
+++ students/hyska_monika/lesson_00_the_setup/hello_world.py
@@ -6,4 +6,13 @@ Python supports many ways to write programs. You can start with a very simple
 imperative code, like this script.
 """
 
-print("Hello world!")
+
+def main():
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    # `python hello_world.py` will run main(), `import hello_world` will not
+    main()
+
+# `import hello_world` will still allow me to run `hello_world.main()`
