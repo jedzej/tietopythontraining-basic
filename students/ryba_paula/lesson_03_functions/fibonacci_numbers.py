be85bac075eccc5a93f833ddfa4db871d95844fc
Paula Ryba <paula.ryba@sonymobile.com>
2018-03-15 15:53:53 +0100

    Fix try-except blocks in lesson_03_functions

--- students/ryba_paula/lesson_03_functions/fibonacci_numbers.py
+++ students/ryba_paula/lesson_03_functions/fibonacci_numbers.py
@@ -5,13 +5,19 @@ def fib(n):
         return fib(n - 1) + fib(n - 2)
 
 
-while True:
-    try:
+def input_validation():
+    while True:
         n = int(input("Input a int number: "))
         if n < 0:
             print("The number must be non-negative")
             continue
         print("The result is:", fib(n))
+        break
+
+
+while True:
+    try:
+        input_validation()
         break
     except ValueError:
         print("The number must be an integer")
