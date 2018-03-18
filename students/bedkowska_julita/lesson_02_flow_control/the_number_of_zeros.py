4fcebe183feb7abb86823d6794a47950eb53f011
Julita Bedkowska <julitabedkowska@gmail.com>
2018-03-18 19:37:12 +0100

    Missing whitespace around arithmetic operator

--- students/bedkowska_julita/lesson_02_flow_control/the_number_of_zeros.py
+++ students/bedkowska_julita/lesson_02_flow_control/the_number_of_zeros.py
@@ -2,7 +2,7 @@ numbers = int(input('Give the number of number: '))
 
 result = 0
 for i in range(numbers):
-    num = int(input('Give the '+str(i + 1)+' number: '))
+    num = int(input('Give the ' + str(i + 1) + ' number: '))
     if num == 0:
         result += 1
 
