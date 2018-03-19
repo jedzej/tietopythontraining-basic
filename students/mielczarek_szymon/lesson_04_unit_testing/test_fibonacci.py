683fa4148ea1daa78e24b428b123aaa93a763cb7
Szymon Mielczarek <szymon.mielczarek@tieto.com>
2018-03-19 18:29:54 +0100

    Lesson 04: Test Fibonacci numbers

---
+++ students/mielczarek_szymon/lesson_04_unit_testing/test_fibonacci.py
@@ -0,0 +1,31 @@
+import sys
+import pytest
+from lesson_03_functions.fibonacci_numbers import fib
+sys.path.append('..')
+
+
+class TestFibonacci:
+    def test_value_0(self):
+        assert fib(0) == 0
+
+    def test_value_1(self):
+        assert fib(1) == 1
+
+    def test_value_2(self):
+        assert fib(2) == 1
+
+    def test_value_21(self):
+        assert fib(21) == 10946
+
+    def test_negative_value(self):
+        with pytest.raises(RuntimeError) as exception:
+            fib(-1)
+        assert 'maximum recursion depth exceeded' in str(exception.value)
+
+    def test_non_integer(self):
+        with pytest.raises(TypeError):
+            fib('aoeu')
+
+    def test_on_none(self):
+        with pytest.raises(TypeError):
+            fib(None)
