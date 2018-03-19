22c9203b903359e1efbe8044b4c507c7f6f3995b
Jakub Brach <jakub.i.brach@gmail.com>
2018-03-20 00:09:12 +0100

    Stickler corrected

--- students/BRACH_Jakub/lesson_02_Flow_Control/Chocolate.py
+++ students/BRACH_Jakub/lesson_02_Flow_Control/Chocolate.py
@@ -2,7 +2,7 @@ n = int(input())
 m = int(input())
 k = int(input())
 
-if (k % n == 0 and m >= (k // n)) or (k % m == 0 and n > k / /m):
+if (k % n == 0 and m >= (k // n)) or (k % m == 0 and n > k // m):
     print("YES")
 else:
     print("NO")
