aeffa90e2bc8de2bd29ab07933a3ada178eeb9bf
Graheb <grazyna.hebdowska@gmail.com>
2018-03-19 08:21:49 +0100

    Lesson 3

---
+++ students/Hebdowska_Grazyna/lesson_03_functions/TheLenghtOfTheSegments.py
@@ -0,0 +1,12 @@
+import math
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    
+x1 = int(input("x1: "))
+y1 = int(input("y1: "))
+x2 = int(input("x2: "))
+y2 = int(input("y2: "))
+
+print(distance(x1, y1, x2, y2))
+
