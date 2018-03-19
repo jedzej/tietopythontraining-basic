8cadaa13e896bbdde4642da3949e42473aa29f5e
myszkpaw <37329235+myszkpaw@users.noreply.github.com>
2018-03-19 13:25:43 +0100

    Lesson 2

---
+++ students/myszko_pawel/lesson_02_flow_control/02_Sign function.py
@@ -0,0 +1,12 @@
+# For the given integer X print 1 if it's positive, -1 if it's negative, or 0 if it's equal to zero.
+# Try to use the cascade if-elif-else for it.
+
+# Read an integer:
+a = int(input())
+# Print a value:
+if a > 0:
+    print(1)
+elif a < 0:
+    print(-1)
+else:
+    print (0)
