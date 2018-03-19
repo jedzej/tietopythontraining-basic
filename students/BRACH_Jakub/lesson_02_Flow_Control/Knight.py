f2c955f15d63b33830e2abe9d66ba28660563fac
Jakub Brach <jakub.i.brach@gmail.com>
2018-03-19 23:18:53 +0100

    Second Part

---
+++ students/BRACH_Jakub/lesson_02_Flow_Control/Knight.py
@@ -0,0 +1,17 @@
+x_base = int(input())
+y_base = int(input())
+x_move = int(input())
+y_move = int(input())
+
+x_delta = x_base - x_move
+y_delta = y_base - y_move
+
+if x_delta < 0:
+    x_delta = x_delta * (-1)
+if y_delta < 0:
+    y_delta = y_delta * (-1)
+
+if y_delta == 1 and  x_delta ==2 or y_delta == 2 and  x_delta ==1:
+    print("YES")
+else:
+    print("NO")
