e40f2b461b63b2f55a19c7f249b0433347894206
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-03-19 15:17:43 +0100

    lesson03 one more fix

--- students/jemielity_kamil/lesson_03_functions/the_length_of_the_segment.py
+++ students/jemielity_kamil/lesson_03_functions/the_length_of_the_segment.py
@@ -2,7 +2,7 @@ import math
 
 
 def distance(x1, y1, x2, y2):
-    result = math.sqrt((math.pow(x2 - x1, 2))+(math.pow(y2 - y1, 2)))
+    result = math.sqrt((math.pow(x2 - x1, 2)) + (math.pow(y2 - y1, 2)))
     return result
 
 
