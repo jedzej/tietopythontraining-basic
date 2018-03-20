2ad45916eaefcfdb132323ba240ed934370a6bda
olszebar <bartosz.olszewski@tieto.com>
2018-03-20 07:32:24 +0100

    Update the_length_of_the_segment.py

--- students/olszewski_bartosz/lesson_03_functions/the_length_of_the_segment.py
+++ students/olszewski_bartosz/lesson_03_functions/the_length_of_the_segment.py
@@ -5,4 +5,6 @@ def length_of_segment(x1, y1, x2, y2):
     length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return length
 
-print(length_of_segment(float(input()), float(input()), float(input()), float(input())))
+
+print(length_of_segment(float(input()), float(input()),
+                        float(input()), float(input())))
