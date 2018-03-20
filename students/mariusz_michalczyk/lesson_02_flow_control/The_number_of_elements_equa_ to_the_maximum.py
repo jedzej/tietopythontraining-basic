b46a7101fe6b8eef604420ddd7820fb5d9497d39
Unknown <marioosh11@gmail.com>
2018-03-20 13:28:31 +0100

    Fixes for number_of_elements_equal_to_the_maximum

--- students/mariusz_michalczyk/lesson_02_flow_control/The_number_of_elements_equa_ to_the_maximum.py
+++ students/mariusz_michalczyk/lesson_02_flow_control/The_number_of_elements_equa_ to_the_maximum.py
@@ -1,13 +1,11 @@
-# NOT FINISHED
-
-equal_nrs, largest, i = 0, 0, 0
-while True:
-    n = abs(int(input()))
-    if n > largest:
-        largest = n
-    if n == largest:
-        equal_nrs += 1
-        print("+")
-    if n == 0:
-        break
-print(equal_nrs)
+counter = 0
+maximum_nr = 0
+entered_nr = -1
+while entered_nr != 0:
+    entered_nr = int(input())
+    if entered_nr > maximum_nr:
+        maximum_nr = entered_nr
+        counter = 1
+    elif entered_nr == maximum_nr:
+        counter += 1
+print(counter)
