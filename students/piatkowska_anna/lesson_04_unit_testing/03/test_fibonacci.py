fa78a61662a32955bae1d38ef113c1827ac62054
gubryann <anna.piatkowska@tieto.com>
2018-03-21 15:20:35 +0100

    lesson 04 unit testing

---
+++ students/piatkowska_anna/lesson_04_unit_testing/03/test_fibonacci.py
@@ -0,0 +1,27 @@
+# Test your code from Fibonacci numbers from previous lesson.
+# You can use pytest or unittest.
+from fibonacci_numbers import fib
+import unittest
+
+
+class FibonacciTest(unittest.TestCase):
+
+    def test_value_error_raised_when_string_is_on_input(self):
+        with self.assertRaises(ValueError):
+            fib('string')
+
+    def test_returns_21_when_called_on_8(self):
+        self.assertEqual(fib(8), 21)
+
+    def test_returns_5_when_called_on_5(self):
+        self.assertEqual(fib(5.5), 5)
+
+    def test_returns_1_when_called_on_1(self):
+        self.assertEqual(fib(1), 1)
+
+    def test_returns_1_when_called_on_2(self):
+        self.assertEqual(fib(2), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
