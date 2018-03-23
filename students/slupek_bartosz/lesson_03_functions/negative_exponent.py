bd23bd8b2df2fee4f943ba6c4d8672d4a5ac3bee
vSupas <36338390+vSupas@users.noreply.github.com>
2018-03-23 08:33:42 +0100

    Add lesson 3

---
+++ students/slupek_bartosz/lesson_03_functions/negative_exponent.py
@@ -0,0 +1,12 @@
+def power(a, n):
+    result = 1
+    if n > 0:
+        for i in range(n):
+            result *= a
+    elif n < 0:
+        a = 1 / a
+        for i in range(abs(n)):
+            result *= a
+    return result
+
+print(power(float(input()), int(input())))
