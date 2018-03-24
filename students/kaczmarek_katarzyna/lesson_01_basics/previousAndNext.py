79b97f4dbc8775908723b7613c3001d19af489a4
Kasia Kaczmarek <katarzyna.kubainska@wp.pl>
2018-03-24 17:59:20 +0100

    I jeszcze jedna poprawka...

--- students/kaczmarek_katarzyna/lesson_01_basics/previousAndNext.py
+++ students/kaczmarek_katarzyna/lesson_01_basics/previousAndNext.py
@@ -1,6 +1,6 @@
 number = int(input("Number: "))
 
-print("The next number for the number " + str(number)
-      + " is " + str(number + 1) + ".")
-print("The previous number for the number " + str(number)
-      + " is " + str(number - 1) + ".")
+print("The next number for the number " + str(number) +
+      " is " + str(number + 1) + ".")
+print("The previous number for the number " + str(number) +
+      " is " + str(number - 1) + ".")
