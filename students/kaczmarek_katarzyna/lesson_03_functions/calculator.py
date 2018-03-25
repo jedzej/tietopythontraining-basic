0024e2ea239b09c94327da6f837624c500d0813b
Kasia Kaczmarek <katarzyna.kubainska@wp.pl>
2018-03-25 22:54:47 +0200

    Lesson 3 - All issues

---
+++ students/kaczmarek_katarzyna/lesson_03_functions/calculator.py
@@ -0,0 +1,83 @@
+def input_variables():
+    while True:
+        try:
+            variable1 = float(input("Type first number: "))
+            variable2 = float(input("Type second number: "))
+            return variable1, variable2
+        except ValueError:
+            print("Wrong value! Try again!")
+
+
+def add():
+    variable1, variable2 = input_variables()
+    print("Operation: ADD. Result: ", variable1 + variable2)
+
+
+def subtract():
+    variable1, variable2 = input_variables()
+    print("Operation: SUBTRACT. Result: ", variable1 - variable2)
+
+
+def multiply():
+    variable1, variable2 = input_variables()
+    print("Operation: MULTIPLY. Result: ", variable1 * variable2)
+
+
+def divide():
+    try:
+        variable1, variable2 = input_variables()
+        print("Operation: DIVIDE. Result: ", variable1 / variable2)
+    except ZeroDivisionError:
+        print("A second argument of a division can't be zero. Try again.")
+
+
+def power():
+    variable1, variable2 = input_variables()
+    print("Operation: POWER - Result: ", variable1 ** variable2)
+
+
+def print_help():
+    print("Available options:")
+    print("a - add")
+    print("s - subtract")
+    print("m - multiply")
+    print("d - divide")
+    print("p - power")
+    print("h,? - help")
+    print("q - QUIT")
+
+
+def start():
+    print("Welcome to the calculator.")
+    print_help()
+
+    while True:
+        option = input("Enter option: ")
+
+        if option == "a":
+            add()
+
+        elif option == "s":
+            subtract()
+
+        elif option == "m":
+            multiply()
+
+        elif option == "d":
+            divide()
+
+        elif option == "p":
+            power()
+
+        elif option == "h" or option == "?":
+            print_help()
+
+        elif option == "q":
+            print("GOOD BYE")
+            break
+
+        else:
+            print("Wrong option. Try again.")
+
+
+start()
