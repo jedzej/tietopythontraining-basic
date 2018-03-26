b38b7c3dfadfde2f47d562ce27e53f1b4129372c
Adam Wulw <adamwulw@gmail.com>
2018-03-26 12:30:18 +0200

    All lesson 3

---
+++ students/adam_wulw/lesson_03_functions/exponentiation.py
@@ -0,0 +1,10 @@
+def power(a, n):
+    if n > 0:
+        return a * power(a, n-1)
+    else:
+        return 1
+
+
+a = int(input('Enter a\n'))
+n = int(input('Enter n\n'))
+print('Power {0} from {1} is {2}'.format(n, a, power(a, n)))
