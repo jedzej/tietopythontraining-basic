1029f46449a73d22992a9d605f9be61cb7c49f7d
Adam Wulw <adamwulw@gmail.com>
2018-03-26 12:30:18 +0200

    All lesson 3

---
+++ students/adam_wulw/lesson_03_functions/negative_exponent.py
@@ -0,0 +1,13 @@
+def power(a, n):
+    ret = 1
+    if n > 0:
+        for i in range(n):
+            ret *= a
+        return ret
+    else:
+        for i in range(-n):
+            ret *= 1 / a
+        return ret
+a = float(input('Enter a\n'))
+n = int(input('Enter n\n'))
+print('Power {0} from {1} is {2}'.format(n, a, power(a, n)))
