7a1464af6cd7820511c2d3b294c744693ac82078
olszebar <bartosz.olszewski@tieto.com>
2018-03-26 14:02:18 +0200

    Update collatz.py

--- students/olszewski_bartosz/lesson_03_functions/collatz.py
+++ students/olszewski_bartosz/lesson_03_functions/collatz.py
@@ -1,10 +1,10 @@
 def collatz(number):
     if number % 2 == 0:
-        print(number // 2)
-        return number // 2
-    elif number % 2 == 1:
-        print(3 * number + 1)
-        return 3 * number + 1
+        value = number // 2
+    else:
+        value = 3 * number + 1
+    print(value)
+    return value
 
 
 try:
