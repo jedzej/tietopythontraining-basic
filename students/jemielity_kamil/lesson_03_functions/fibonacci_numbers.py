c5ad24cae23817c1018f0e044dcc6e599a910546
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-03-26 11:51:49 +0200

    fibo fixes

--- students/jemielity_kamil/lesson_03_functions/fibonacci_numbers.py
+++ students/jemielity_kamil/lesson_03_functions/fibonacci_numbers.py
@@ -1,14 +1,21 @@
 def fib(n):
-    if n == 1:
-        return 1
-    elif n == 0:
-        return 0
-    elif n < 0:
-        raise ValueError('Wrong value')
-    else:
-        return fib(n - 1) + fib(n - 2)
+    try:
+        n = int(n)
+        if n < 0:
+            raise ValueError('Wrong Value')
+        elif n == 1:
+            return 1
+        elif n == 0:
+            return 0
+
+        else:
+            return fib(n - 1) + fib(n - 2)
+
+    except ValueError:
+        print('Wrong Value')
+        exit()
 
 
 if __name__ == "__main__":
-    number = int(input("Positive integer: "))
+    number = input("Positive integer: ")
     print(fib(number))
