9aa17ea27aa152ce867e921b1bdda00280b1593e
Jakub Brach <jakub.i.brach@gmail.com>
2018-03-26 18:50:47 +0100

    Another part of work

---
+++ students/BRACH_Jakub/lesson_03_Functions/collatz_validation.py
@@ -0,0 +1,16 @@
+# Collatz Sequemce
+def Collatz(number):
+    if number % 2 == 0:
+        retval = number // 2
+    else:
+        retval = 3 * number + 1
+    print(retval)
+    return retval
+
+try:
+    in_data = int(input())
+    while in_data != 1:
+        in_data = Collatz(in_data)
+except ValueError:
+    print("Error:Input must be integer!")
+
