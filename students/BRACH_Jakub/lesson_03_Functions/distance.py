b4dcb71a0f76e2950d472429ba9397be01b6aa3d
Jakub Brach <jakub.i.brach@gmail.com>
2018-03-27 00:03:45 +0200

    PEP8 Applied....again

--- students/BRACH_Jakub/lesson_03_Functions/distance.py
+++ students/BRACH_Jakub/lesson_03_Functions/distance.py
@@ -4,7 +4,7 @@ from math import sqrt, pow
 def distance(x1, y1, x2, y2):
     return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
 
-	
+
 x1 = float(input())
 y1 = float(input())
 x2 = float(input())
