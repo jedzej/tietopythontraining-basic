963465d46e8177d86148ea181e692e270373b1d2
Jakub Brach <jakub.i.brach@gmail.com>
2018-03-26 15:56:43 +0100

    Collatz.py added

--- students/BRACH_Jakub/lesson_03_Functions/collatz.py
+++ students/BRACH_Jakub/lesson_03_Functions/collatz.py
@@ -1,2 +1,13 @@
 #Collatz Sequemce
+def Collatz(number):
+    if number % 2 == 0:
+        retval = number // 2
+    else:
+        retval =  3 * number +1
+    print(retval)
+    return retval
 
+
+in_data = int(input())
+while in_data != 1:
+    in_data = Collatz(in_data)
