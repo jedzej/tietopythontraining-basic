8b4d5cf2b9f32dc3a45dd07a2a1ab56123520390
Michal Mitek <mitek.michal@gmail.com>
2018-03-27 19:56:16 +0200

    Correct E126 PEP violation

--- students/mitek_michal/lesson_03_functions/length_of_the_segment.py
+++ students/mitek_michal/lesson_03_functions/length_of_the_segment.py
@@ -9,9 +9,9 @@ def calculate_length_of_the_segment(x1, y1, x2, y2):
 
 
 x1_input, x2_input, y1_input, y2_input = float(input("Provide x1 ")),\
-                                    float(input("Provide x2 ")),\
-                                    float(input("Provide y1 ")),\
-                                    float(input("Provide y2 "))
+                                            float(input("Provide x2 ")),\
+                                            float(input("Provide y1 ")),\
+                                            float(input("Provide y2 "))
 
 if __name__ == '__main__':
     calculate_length_of_the_segment(x1_input, x2_input, y1_input, y2_input)
