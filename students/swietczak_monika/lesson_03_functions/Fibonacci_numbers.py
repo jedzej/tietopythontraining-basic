b1569f8587d23fb757e8cfc2a58c556d6a6880a8
Swietczak Monika <monika.swietczak@tieto.com>
2018-03-29 10:20:04 +0200

    Add lesson 3 solutions second time.

---
+++ students/swietczak_monika/lesson_03_functions/Fibonacci_numbers.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+number = int(input("Enter a number: "))
+print(fib(number))
