7376c6b7bdc39bbe8b4995df1ac45c486a3cc2b5
vSupas <36338390+vSupas@users.noreply.github.com>
2018-04-02 11:18:19 +0200

    Update fibonacci_numbers.py

--- students/slupek_bartosz/lesson_03_functions/fibonacci_numbers.py
+++ students/slupek_bartosz/lesson_03_functions/fibonacci_numbers.py
@@ -1,5 +1,9 @@
 def fib(n):
-    if n == 1 or n == 2:
+    if n < 0:
+        print("Error")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
         return 1
     return fib(n - 1) + fib(n - 2)
 
