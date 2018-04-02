71852be2c447ff993dbdbbe81ba7ddac4bc4d29b
vSupas <36338390+vSupas@users.noreply.github.com>
2018-04-02 11:42:50 +0200

    Update Calculator.py

--- students/slupek_bartosz/lesson_03_functions/Calculator.py
+++ students/slupek_bartosz/lesson_03_functions/Calculator.py
@@ -1,36 +1,36 @@
 def input_operands:
     operand1 = int(input("Input 1st operand: "))
-	operand2 = int(input("Input 2nd operand: "))
-	print("Result: ")
-	return operand1, operand2
+    operand2 = int(input("Input 2nd operand: "))
+    print("Result: ")
+    return operand1, operand2
 
 def multiply():
     print("MULTIPLY")
-    var1 , var2 = input_operands()
+    var1, var2 = input_operands()
     print(var1 *var2)
 
 
 def add():
     print("ADDING")
-    var1 , var2 = input_operands()
+    var1, var2 = input_operands()
     print(var1 + var2)
 
 
 def subtract():
     print("SUBTRACT")
-    var1 , var2 = input_operands()
+    var1, var2 = input_operands()
     print(var1 - var2)
 
 
 def divide():
     print("DIVIDE")
-    var1 , var2 = input_operands()
+    var1, var2 = input_operands()
     print(var1 / var2)
 
 
 def power():
     print("POWER")
-    var1 , var2 = input_operands()
+    var1, var2 = input_operands()
     print(var1 ** var2)
 
 
