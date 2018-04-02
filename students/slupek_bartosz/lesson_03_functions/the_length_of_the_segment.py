a6687cc3c779c03b8b41cae9e16d714967a0f824
vSupas <36338390+vSupas@users.noreply.github.com>
2018-04-02 11:48:06 +0200

    Update the_length_of_the_segment.py

--- students/slupek_bartosz/lesson_03_functions/the_length_of_the_segment.py
+++ students/slupek_bartosz/lesson_03_functions/the_length_of_the_segment.py
@@ -2,9 +2,11 @@ from math import sqrt
 
 
 def length_of_segment(x1, y1, x2, y2):
-    length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    try:
+        length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    except ValueError:
+        print("You have to input a real number")
     return length
-
 
 print(length_of_segment(float(input()), float(input()),
                         float(input()), float(input())))
