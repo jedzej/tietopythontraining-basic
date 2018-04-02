861851a441bc5d8e167bc877e2994aa81a56f17a
herezmar <marta.herezo@gmail.com>
2018-04-02 15:51:28 +0200

    Lesson_01 cd.
    Small changes in clock_face_1

--- students/marta_herezo/lesson_01/lesson_02/clock_face_1.py
+++ students/marta_herezo/lesson_01/lesson_02/clock_face_1.py
@@ -7,13 +7,7 @@ M = int(input())
 print('Give seconds 0 ≤ S < 60:')
 S = int(input())
 
-sum = 1 + M/60 + S/3600
+sum = H + M/60 + S/3600
 degrees = 90*sum/3
 
-print(str(degrees))
-#sum_m = sum_m/60
-#print(sum_m)
-#sumS = S/3600
-#print(sumS)
-
-print('It is: ' + str(sum))
+print(str(H) + ':' + str(M) + ':' + str(S) + ' = ' + str(degrees) + '[degrees]')
