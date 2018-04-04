433c39b7d8eaab9a05fa2014918d1f2a0b1cd268
saletseb <sebastian.saleta@tieto.com>
2018-04-04 08:49:42 +0200

    Solve sticker problems and delete unused if statement

--- students/saleta_sebastian/lesson_03_functions/collatz.py
+++ students/saleta_sebastian/lesson_03_functions/collatz.py
@@ -1,13 +1,11 @@
 def collatz(number):
-    
-    if number < 0:
-        raise ValueError('Only positive numbers ')
+
     if number % 2 == 0:
-        print(number // 2)
+        print (number // 2)
         return number // 2
     else:
-        print(3 * number + 1)
-        return 3 * number +1
+        print (3 * number + 1)
+        return 3 * number + 1
 
 
 def main():
