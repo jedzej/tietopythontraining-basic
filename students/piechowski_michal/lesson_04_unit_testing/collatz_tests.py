2eb4df46f5bb44d7b2a34f1b2b2d681a6bf2d4a5
Michal Piechowski <michal.a.piechowski@tieto.com>
2018-04-05 09:54:18 +0200

    Fix code according to review comments

--- students/piechowski_michal/lesson_04_unit_testing/collatz_tests.py
+++ students/piechowski_michal/lesson_04_unit_testing/collatz_tests.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import unittest
-parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(parent_dir_name + "/lesson_03_functions")
-
-from collatz_sequence import collatz
+from students.piechowski_michal.lesson_03_functions.collatz_sequence import collatz
 
 
 class CollatzTest(unittest.TestCase):
