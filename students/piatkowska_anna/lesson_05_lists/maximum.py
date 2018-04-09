99f9b4e89911e6c0fbfbb16813ae300dba4423c9
gubryann <anna.piatkowska@tieto.com>
2018-04-09 10:51:45 +0200

    Add mising description

--- students/piatkowska_anna/lesson_05_lists/maximum.py
+++ students/piatkowska_anna/lesson_05_lists/maximum.py
@@ -1,4 +1,12 @@
 '''
+Given two integers representing the rows and columns (m×n),
+and subsequent m rows of n elements, find the index position
+of the maximum element and print two numbers representing
+the index (i×j) or the row number and the column number.
+If there exist multiple such elements in different rows,
+print the one with smaller row number.
+If there multiple such elements occur on the same row,
+output the smallest column number.
 '''
 
 
