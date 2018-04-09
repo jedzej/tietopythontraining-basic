75fe55adc50ca5cb5aafd6ca48da039050d8aa12
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-04-09 14:08:16 +0200

    added whitespace

--- students/jemielity_kamil/lesson_05_lists/the_bowling_alley.py
+++ students/jemielity_kamil/lesson_05_lists/the_bowling_alley.py
@@ -6,7 +6,7 @@ bowls = ['I' for i in range(n)]
 for x in range(k):
     range_a, range_b = [int(i) for i in input('Pins: ').split()]
 
-    for i in range(range_a, range_b+1):
+    for i in range(range_a, range_b + 1):
         bowls[i - 1] = '.'
 
 bowls = ''.join(bowls)
