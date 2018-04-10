7b46611f217f66b34e81fd3d4d0767b65593df3a
olszebar <bartosz.olszewski@tieto.com>
2018-04-10 11:21:22 +0200

    lesson04_unit_testing

---
+++ students/olszewski_bartosz/lesson_04_unit_testing/fibonacci_test.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+def test_for_5():
+    assert fib(5) == 5
+
+
+def test_for_15():
+    assert fib(15) == 610
+
+
+def test_raises_type_error():
+    with pytest.raises(TypeError):
+        fib('aeou')
+
+
+def test_non_value():
+    with pytest.raises(TypeError):
+        fib('None')
+
+
+def test_negative_value():
+    with pytest.raises(RecursionError):
+        fib(-1)
