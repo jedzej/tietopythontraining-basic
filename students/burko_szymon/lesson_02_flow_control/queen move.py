32a7c8d7e585ab7344c4aa86835cc9be65772c66
Burko Szymon <szymon.burko@tieto.com>
2018-04-10 13:05:36 +0200

    Lesson 01 and Lesson 02 added

---
+++ students/burko_szymon/lesson_02_flow_control/queen move.py
@@ -0,0 +1,11 @@
+startX = int(input("Give start x position: "))
+startY = int(input("Give start y position: "))
+endX = int(input("Give end x position: "))
+endY = int(input("Give end y position: "))
+
+shiftX = endX - startX
+shiftY = endY - startY
+
+if shiftX == shiftY:
+    print("Yes"
+elif shiftX  
