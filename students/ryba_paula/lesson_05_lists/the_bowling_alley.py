3b94e08ef38e1c03d30934af42adceba2c02b3ad
Paula Ryba <paula.ryba@sonymobile.com>
2018-04-11 10:13:28 +0200

    Add lesson_05_lists solutions

---
+++ students/ryba_paula/lesson_05_lists/the_bowling_alley.py
@@ -0,0 +1,18 @@
+def knock_pins(pins, balls):
+    result = pins * ['I']
+    for i in range(balls):
+        start, stop = [int(x) for x in input("Type start and stop position: ").split()]
+        for j in range(start - 1, stop):
+            result[j] = '.'
+
+    print(''.join(result))
+
+
+def main():
+    a, b = [int(i) for i in input("Input pins and balls: ").split()]
+
+    knock_pins(a, b)
+
+
+if __name__ == '__main__':
+    main()
