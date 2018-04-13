d503cfb5036636141acd26ed86c49639cfa4cf36
Stanislaw Piotrowski <stanislaw.piotrowski@gmail.com>
2018-04-13 14:54:39 +0200

    Lesson 4 completed

---
+++ students/piotrowski_stanislaw/lesson_04_unit_testing/test_fibonacci_rec.py
@@ -0,0 +1,29 @@
+import pytest
+from fibonacci_rec import fib
+#from lesson_03_functions.fibonacci_rec import fib
+
+class TestFibonacciFunction(object):
+    def test_none_as_input_parameter(self):
+        with pytest.raises(TypeError):
+            fib(None)
+
+    def test_string_as_input_parameter(self):
+        with pytest.raises(TypeError):
+            fib('aoeu')
+
+    def test_real_number_as_input_parameter(self):
+        with pytest.raises(TypeError):
+            fib('1.3')
+
+    def test_negative_input_exception(self):
+        with pytest.raises(ValueError):
+            fib(-1)
+
+    def test_zero_as_input_parameter(self):
+        assert fib(0) == 0
+
+    def test_one_as_input_parameter(self):
+        assert fib(1) == 1
+
+    def test_eight_as_input_parameter(self):
+        assert fib(8) == 21
