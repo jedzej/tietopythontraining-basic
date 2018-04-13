b58f0c2898b90893827a92cd050c23d9a3553b57
Izaak Lakatorz <ext-izaak.lakatorz@here.com>
2018-04-13 13:19:24 +0200

    Lesson 05 solutions

---
+++ students/lakatorz_izaak/lesson_05_lists/character_picture_grid.py
@@ -0,0 +1,16 @@
+# Copy the previous grid value, and write code that uses it to print the image.
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for i in range(6):
+    for j in range(9):
+        print(grid[j][i], end='')
+    print('')
