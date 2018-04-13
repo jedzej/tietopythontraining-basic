83c6c96babec8606bfc4a816719a897b220cc721
Michal Piechowski <M.Z.Piechowski@gmail.com>
2018-04-13 21:19:52 +0200

    Fix code according to stickler

--- students/piechowski_michal/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
+++ students/piechowski_michal/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
@@ -3,16 +3,17 @@
 
 def main():
     n = int(input())
-    frequencies_dictionary = {}
+    frequencies_dict = {}
 
     for i in range(0, n):
         for word in input().split():
-            if word in frequencies_dictionary.keys():
-                frequencies_dictionary[word] += 1
+            if word in frequencies_dict.keys():
+                frequencies_dict[word] += 1
             else:
-                frequencies_dictionary[word] = 1
+                frequencies_dict[word] = 1
 
-    frequencies_list = list([-frequency, word] for word, frequency in frequencies_dictionary.items())
+    frequencies_list = list([-frequency, word]
+                            for word, frequency in frequencies_dict.items())
 
     for frequency, word in sorted(frequencies_list):
         print(word)
