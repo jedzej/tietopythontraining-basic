49579dee765baa83dfa7b9a25acc5bd8fa3ce4e1
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-04-13 13:59:17 +0200

    next part of tasks lesson 06

---
+++ students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
@@ -0,0 +1,26 @@
+
+Dictionary = {}
+for i in range(int(input('How many lines of text: '))):
+    sentence = input('Sentence: ').split()
+    for sentence_word in sentence:
+        if sentence_word not in Dictionary.keys():
+            Dictionary[sentence_word] = 1
+        else:
+            Dictionary[sentence_word] += 1
+
+Dictionary = list(Dictionary.items())
+
+for i in range(len(Dictionary)):
+    Dictionary[i] = tuple(reversed(Dictionary[i]))
+
+Dictionary = sorted(Dictionary)
+
+for i in range(len(Dictionary)):
+    Dictionary[i] = list(Dictionary[i])
+    Dictionary[i][0] = -Dictionary[i][0]
+
+for i in range(len(Dictionary)):
+    print(min(Dictionary)[1])
+    Dictionary.remove(min(Dictionary))
+
+
