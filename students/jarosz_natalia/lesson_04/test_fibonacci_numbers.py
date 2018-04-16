6d396888a0125a25c6a496f98f53f6999f1bf180
Natalia Jarosz <natalia.jarosz@tieto.com>
2018-04-16 22:21:17 +0200

    adding lesson_04

---
+++ students/jarosz_natalia/lesson_04/test_fibonacci_numbers.py
@@ -0,0 +1,21 @@
+import unittest
+from lesson_03.fibonacci_numbers import fib
+
+
+class MyTest(unittest.TestCase):
+
+    def test_1(self):
+        self.assertRaises(TypeError, fib, None)
+
+    def test_2(self):
+        self.assertRaises(TypeError, fib, 'aoeu')
+
+    def test_3(self):
+        with self.assertRaises(RecursionError):
+            fib(0)
+
+    def test_4(self):
+        self.assertEqual(fib(7), 13)
+
+if __name__ == '__main__':
+    unittest.main()
