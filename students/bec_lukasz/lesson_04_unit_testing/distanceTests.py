82bf43e11e55fddcb4d70dae4d6c90257f0c821f
lukaszbec <lukaszbec.it@gmail.com>
2018-04-16 14:27:59 +0200

    Removed distanceTests errors for stickler

--- students/bec_lukasz/lesson_04_unit_testing/distanceTests.py
+++ students/bec_lukasz/lesson_04_unit_testing/distanceTests.py
@@ -7,16 +7,6 @@ def distance(x1, y1, x2, y2):
         raise ValueError
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-'''
-x1 = float(input('x1:'))
-y1 = float(input('y1:'))
-x2 = float(input('x2:'))
-y2 = float(input('y2:'))
-
-print(distance(x1, y1, x2, y2))
-
-'''
-
 
 def test_type_error_none_called():
     with pytest.raises(TypeError):
