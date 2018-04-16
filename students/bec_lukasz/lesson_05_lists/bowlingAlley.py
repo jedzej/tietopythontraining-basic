9d70790fd9d6d3afe2bd4ac694c6ed14e7ff1fe8
lukaszbec <lukaszbec.it@gmail.com>
2018-04-16 14:14:55 +0200

    Removed no newline errors for stickler

--- students/bec_lukasz/lesson_05_lists/bowlingAlley.py
+++ students/bec_lukasz/lesson_05_lists/bowlingAlley.py
@@ -1,4 +1,5 @@
-pins_number, balls_rolled = input('Input two numbers: pins number and balls rolled number in four rows').split()
+pins_number, balls_rolled = \
+    input('Input two numbers: pins number and balls rolled number in four rows').split()
 bowling = ['I'] * int(pins_number)
 for i in range(int(balls_rolled)):
     left, right = input().split()
