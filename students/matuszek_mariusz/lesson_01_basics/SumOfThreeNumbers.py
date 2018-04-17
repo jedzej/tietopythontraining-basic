14d2d627407a9f5a85c2ef3d354413d90130177d
Mariusz M <mariusz.matuszek@tieto.com>
2018-04-17 13:19:38 +0200

    Creating script for Problem 1

---
+++ students/matuszek_mariusz/lesson_01_basics/SumOfThreeNumbers.py
@@ -0,0 +1,11 @@
+print('This program reads two numbers and prints their sum')
+
+print('First: ')
+a = int(input())
+print('Second: ')
+b = int(input())
+print('Third')
+c = int(input())
+sum = a+b+c
+
+print('Sum: ' + str(sum))
