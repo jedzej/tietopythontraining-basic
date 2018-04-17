7fef9bd5ddb44f94a255f94ab3116af0acdaeb21
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-04-17 09:21:12 +0200

    picture grid fixes

--- students/jemielity_kamil/lesson_05_lists/character_picture_grid.py
+++ students/jemielity_kamil/lesson_05_lists/character_picture_grid.py
@@ -8,7 +8,8 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-for first_loop in range(0, len(grid[0])):
-    for second_loop in range(0, len(grid)):
-        print(grid[second_loop][first_loop], end="")
-    print()
+for first_loop in range(len(grid[0])):
+    line = ""
+    for second_loop in range(len(grid)):
+        line += grid[second_loop][first_loop]
+    print(line)
