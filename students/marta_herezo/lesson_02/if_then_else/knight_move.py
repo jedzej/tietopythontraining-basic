692764ee3fb3bb03610b29f9fe99ce650c716018
ApusAlfa <36867072+ApusAlfa@users.noreply.github.com>
2018-04-17 10:27:05 +0200

    Update knight_move.py

--- students/marta_herezo/lesson_02/if_then_else/knight_move.py
+++ students/marta_herezo/lesson_02/if_then_else/knight_move.py
@@ -1,5 +1,7 @@
-# The program directed corrected or not corrected move of bishop on the chessboard.
-# (x0, y0) - start position, (x, y) - new position on the chessboard.
+# The program directed corrected or not corrected
+# move of bishop on the chessboard.
+# (x0, y0) - start position, (x, y) - new position
+# on the chessboard.
 
 print('Enter start posiotion for bishop, column x0: ')
 x0 = int(input())
