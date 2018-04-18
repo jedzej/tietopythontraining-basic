a38d297814cb8acaeb20f72649fe0b312a22b998
zelazmon <hyska.monika@gmail.com>
2018-04-18 14:21:52 +0200

    delete space

--- students/hyska_monika/lesson_05_ Lists_list_comprehensions/TheBowlingAlley.py
+++ students/hyska_monika/lesson_05_ Lists_list_comprehensions/TheBowlingAlley.py
@@ -4,15 +4,14 @@
 pins_number = int((input("How many pins?: ")))
 throws_number = int((input("How many throwss?: ")))
 
-pins_list =  ["I" for i in range(pins_number)]
+pins_list = ["I" for i in range(pins_number)]
 print(pins_list)
-
 
 for i in range(throws_number):
     print("Put range throws pins: ")
     throw_pins_from = int((input()))
     throw_pins_to = int((input()))
-    #pins_list = ["." for elem in range(throw_pins_from, throw_pins_to)]
+    # pins_list = ["." for elem in range(throw_pins_from, throw_pins_to)]
     for elem in range(throw_pins_from - 1, throw_pins_to):
         pins_list[elem] = "."
 print(pins_list)
