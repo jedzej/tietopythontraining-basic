4a6b859230081e4644d1ee2e1bda5cdacfcfcbfc
lukaszbec <lukaszbec.it@gmail.com>
2018-04-19 11:53:50 +0200

    Calculator modified

--- students/bec_lukasz/lesson_03_functions/calculator_refactored.py
+++ students/bec_lukasz/lesson_03_functions/calculator_refactored.py
@@ -1,47 +1,44 @@
-
-
 def adding(first, second):
-    return first + second
+    return int(first) + int(second)
 
 
 def subtract(first, second):
-    return first - second
+    return int(first) - int(second)
 
 
 def multiply(first, second):
-    return first * second
+    return int(first) * int(second)
 
 
 def divide(first, second):
     try:
-        return first / second
+        return int(first) / int(second)
     except ZeroDivisionError:
         print('Cannot divide by zero')
 
 
 def power(first, second):
-    return first**second
+    return int(first)**int(second)
 
 
-def help():
-    print("HELP")
-    print("add() - add")
-    print("subtract() - subtract")
-    print("multiply() - multiply")
-    print("divide() - divide")
-    print("power() - power")
-
-
-print("Input 1st operand:")
-add_var_1 = int(input())
-
-print("Input 2nd operand:")
-add_var_2 = int(input())
-
-
-help()
-print(adding(add_var_1, add_var_2))
-print(subtract(add_var_1, add_var_2))
-print(multiply(add_var_1, add_var_2))
-print(divide(add_var_1, add_var_2))
-print(power(add_var_1, add_var_2))
+while True:
+    firstNumber = input('Enter first number or \'q\' to quit: ')
+    if firstNumber == 'q':
+        break
+    operationType = input('Enter one of operands + - * / ** or \'q\' to quit')
+    if operationType == 'q':
+        break
+    secondNumber = input('Second Number or \'q\': ')
+    if str(secondNumber) == 'q':
+        print('opp type')
+        break
+    if operationType == '+':
+        print(adding(firstNumber, secondNumber))
+    if operationType == '-':
+        print(subtract(firstNumber, secondNumber))
+    if operationType == '*':
+        print(multiply(firstNumber, secondNumber))
+    if operationType == '/':
+        print(divide(firstNumber, secondNumber))
+    if operationType == '**':
+        print(power(firstNumber, secondNumber))
