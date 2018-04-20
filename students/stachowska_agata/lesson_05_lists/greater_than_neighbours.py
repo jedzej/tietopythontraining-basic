984f9b23f73d53265ab0005425f9171098cfb7ff
Stachowska Agata <agata.stachowska@tieto.com>
2018-04-20 14:11:29 +0200

    fixed stickler error

--- students/stachowska_agata/lesson_05_lists/greater_than_neighbours.py
+++ students/stachowska_agata/lesson_05_lists/greater_than_neighbours.py
@@ -2,7 +2,7 @@ n = [int(i) for i in input().split()]
 quantity = 0
 
 for i in range(1, len(n) - 1):
-    if (n[i] > n[i-1]) and (n[i] > n[i+1]):
+    if (n[i] > n[i - 1]) and (n[i] > n[i + 1]):
         quantity += 1
 
 print(quantity)
