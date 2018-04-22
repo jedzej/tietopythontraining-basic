6225ed37cefae1b85ee48443b391541a35bfad34
Aleksander Kulon <olo3@poczta.onet.pl>
2018-04-22 21:00:29 +0200

    pep8 fixes
    Signed-off-by: Aleksander Kulon <olo3@poczta.onet.pl>

--- students/kulon_aleksander/lesson_05/chess_board.py
+++ students/kulon_aleksander/lesson_05/chess_board.py
@@ -3,8 +3,8 @@ tab = []
 for i in range(int(n)):
     tab.append(['* '] * int(m))
     for j in range(int(m)):
-        if i%2 == 0 and j%2 == 0:
+        if i % 2 == 0 and j % 2 == 0:
             tab[i][j] = '. '
-        elif i%2 and j%2:
+        elif i % 2 and j % 2:
             tab[i][j] = '. '
     print(''.join(tab[i]))
