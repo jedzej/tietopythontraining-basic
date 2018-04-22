7e46caef9addf95cad7ad544fee2585fb221cd72
Mariusz <mariusz.matuszek@tieto.com>
2018-04-22 23:29:39 +0200

    3 new script added

---
+++ students/matuszek_mariusz/lesson_02_flow_control/AddingFactorials.py
@@ -0,0 +1,8 @@
+n = int(input('n = '))
+
+result = 0
+
+for i in range(1, n + 1):
+    a = 1
+    a *= i
+result += a
---
+++ students/matuszek_mariusz/lesson_02_flow_control/Factorial.py
@@ -0,0 +1,8 @@
+n = int(input('n = '))
+
+a = 1
+
+for i in range(1, n + 1):
+    a *= i
+
+print(a)
