7afd5c088b676c391d7cd1a1343bb242a6afffba
Mariusz M <mariusz.matuszek@tieto.com>
2018-04-23 13:42:53 +0200

    Added 3 scripts

--- students/matuszek_mariusz/lesson_02_flow_control/AddingFactorials.py
+++ students/matuszek_mariusz/lesson_02_flow_control/AddingFactorials.py
@@ -1,8 +1,10 @@
 n = int(input('n = '))
 
+a = 1
 result = 0
 
-for i in range(1, n + 1):
-    a = 1
-    a *= i
-result += a
+for i in range(n):
+    a = a + (a * i)
+    result += a
+
+print(result)
---
+++ students/matuszek_mariusz/lesson_02_flow_control/Ladder.py
@@ -0,0 +1,7 @@
+n = int(input('n <=9, n = '))
+
+result = ""
+
+for i in range(1, n + 1):
+    result = result + str(i)
+    print(result, sep="")
