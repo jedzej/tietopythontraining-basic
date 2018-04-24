f7de5814c4d7c680388c82caa1cb1236e8f3f3f6
Karina <karina.nartowska@gmail.com>
2018-04-24 17:16:37 +0200

    Changes after review

--- students/nartowska_karina/lesson_06_dicts_tuples_sets_args_kwargs/the_number_of_distinct_words_in_some_text.py
+++ students/nartowska_karina/lesson_06_dicts_tuples_sets_args_kwargs/the_number_of_distinct_words_in_some_text.py
@@ -1,6 +1,22 @@
+def the_number_of_distinct_words(text):
+    words = {}
+    for i in range(len(text)):
+        new_text = text[i].split()
+        for j in new_text:
+            if j not in words.keys():
+                words[j] = 1
+    words_amount = len(words)
+    return words_amount
+
+
 def main():
+    print("Enter amount of lines and text:")
+    lines_of_text = int(input())
+    data = []
+    for i in range(lines_of_text):
+        data.append(input())
     print("Result:")
-    print(len(set(input().split())))
+    print(the_number_of_distinct_words(data))
 
 
 if __name__ == '__main__':
