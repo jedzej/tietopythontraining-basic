30036672dbd3249a87fd0b0be4a897283a609b12
wachulec_maria <maria.wachulec@tieto.com>
2018-04-25 16:45:08 +0200

    Lesson 6 +

--- students/wachulec_maria/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
+++ students/wachulec_maria/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
@@ -5,7 +5,8 @@ def frequency_analysis():
         for j in words:
             words_frequency.setdefault(j, 0)
             words_frequency[j] += 1
-    frequency_list = sorted(sorted(list(words_frequency.items()), key=lambda elem: elem[0], reverse=False),
+    frequency_list = sorted(sorted(list(words_frequency.items()),
+                            key=lambda elem: elem[0], reverse=False),
                             key=lambda elem: elem[1], reverse=True)
     print(' \n'.join([elem[0] for elem in frequency_list]))
 
