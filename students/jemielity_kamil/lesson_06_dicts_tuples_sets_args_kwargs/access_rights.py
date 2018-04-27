4853e4335445d203ee7b5c26e07aed21aef19c3c
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-04-27 08:37:47 +0200

    access rights fix

--- students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/access_rights.py
+++ students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/access_rights.py
@@ -1,13 +1,13 @@
 
-Dictionary = {}
+dictionary = {}
 Perms = {'read': 'R', 'write': 'W', 'execute': 'X'}
 for i in range(int(input('Number of files: '))):
     file_name, *permissions = (input("File name and permissions: ")).split()
-    Dictionary[file_name] = permissions
+    dictionary[file_name] = permissions
 
 for j in range(int(input('How many actions on files: '))):
     action, file = (input("Action and file: ")).split()
-    if Perms[action] in Dictionary[file]:
+    if Perms[action] in dictionary[file]:
         print('OK')
     else:
         print('Access denied')
