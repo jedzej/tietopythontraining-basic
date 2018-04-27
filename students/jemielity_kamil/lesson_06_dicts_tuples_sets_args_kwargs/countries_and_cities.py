729202d4d0871bb42e1cfbed8161ecd5e6fbdc75
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-04-27 08:39:30 +0200

    countries and cities fix

--- students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/countries_and_cities.py
+++ students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/countries_and_cities.py
@@ -1,10 +1,10 @@
-Countries = dict()
+countries = dict()
 for i in range(int(input('How many countries: '))):
     country, *cities = input("Country and cities: ").split(" ")
-    Countries[country] = cities
+    countries[country] = cities
 
 for j in range(int(input('Number of cities: '))):
     city = input("City: ")
-    for country_key in Countries.keys():
-        if city in Countries[country_key]:
+    for country_key in countries.keys():
+        if city in countries[country_key]:
             print(country_key)
