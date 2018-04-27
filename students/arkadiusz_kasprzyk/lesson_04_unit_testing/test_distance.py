32a299f91b0a324d71fda058ac5e847b92e01216
RCanDo <akasp@interia.pl>
2018-04-27 15:41:26 +0200

    On branch adziu/lesson_05_lists  Changes to be committed: 	modified:   students/arkadiusz_kasprzyk/lesson_04_unit_testing/test_collatz.py 	modified:   students/arkadiusz_kasprzyk/lesson_04_unit_testing/test_distance.py 	modified:   students/arkadiusz_kasprzyk/lesson_04_unit_testing/test_fibonacci.py

--- students/arkadiusz_kasprzyk/lesson_04_unit_testing/test_distance.py
+++ students/arkadiusz_kasprzyk/lesson_04_unit_testing/test_distance.py
@@ -1,9 +1,5 @@
 import pytest
 
-import math
-
-epsilon = 1e-13
-
 
 def distance(x1, y1, x2, y2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
@@ -16,18 +12,18 @@ class TestClass(object):
             distance(None, 0, 0, 1)
 
     def test_value_error(self):
-        with pytest.raises(TypeError):
+        with pytest.raises(ValueError):
             distance('aoeu', 0, 0, 1)
 
     def test_corners(self):
-        assert math.fabs(distance(0, 0, 0, 0) - 0) < epsilon
+        assert distance(0, 0, 0, 0) == 0
         assert distance(-1, -2, -2, -1) > 0
-        assert math.fabs(distance(0, 1, 0, 2) - 1) < epsilon
-        assert math.fabs(distance(1, 0, 2, 0) - 1) < epsilon
+        assert distance(0, 1, 0, 2) == 1
+        assert distance(1, 0, 2, 0) == 1
 
     def test_typical(self):
-        assert math.fabs(distance(0, 4, 3, 0) - 5) < epsilon
+        assert distance(0, 4, 3, 0) == 5
         assert distance(1, 2, 2, 1) > 0
 
     def test_order(self):
-        assert math.fabs(distance(0, 1, 1, 0) - distance(1, 0, 0, 1)) < epsilon
+        assert distance(0, 1, 1, 0) == distance(1, 0, 0, 1)
