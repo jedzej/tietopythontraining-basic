b4c9100a8d236d742ae2118ade1e465889223658
Jemielity Kamil <kamil.jemielity@tieto.com>
2018-04-27 08:49:41 +0200

    frequency analysis fix

--- students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
+++ students/jemielity_kamil/lesson_06_dicts_tuples_sets_args_kwargs/frequency_analysis.py
@@ -1,24 +1,24 @@
 
-Dictionary = {}
+dictionary = {}
 for i in range(int(input('How many lines of text: '))):
     sentence = input('Sentence: ').split()
     for sentence_word in sentence:
-        if sentence_word not in Dictionary.keys():
-            Dictionary[sentence_word] = 1
+        if sentence_word not in dictionary.keys():
+            dictionary[sentence_word] = 1
         else:
-            Dictionary[sentence_word] += 1
+            dictionary[sentence_word] += 1
 
-Dictionary = list(Dictionary.items())
+dictionary = list(dictionary.items())
 
-for i in range(len(Dictionary)):
-    Dictionary[i] = tuple(reversed(Dictionary[i]))
+for i in range(len(dictionary)):
+    dictionary[i] = tuple(reversed(dictionary[i]))
 
-Dictionary = sorted(Dictionary)
+dictionary = sorted(dictionary)
 
-for i in range(len(Dictionary)):
-    Dictionary[i] = list(Dictionary[i])
-    Dictionary[i][0] = -Dictionary[i][0]
+for i in range(len(dictionary)):
+    dictionary[i] = list(dictionary[i])
+    dictionary[i][0] = -dictionary[i][0]
 
-for i in range(len(Dictionary)):
-    print(min(Dictionary)[1])
-    Dictionary.remove(min(Dictionary))
+for i in range(len(dictionary)):
+    print(min(dictionary)[1])
+    dictionary.remove(min(dictionary))
