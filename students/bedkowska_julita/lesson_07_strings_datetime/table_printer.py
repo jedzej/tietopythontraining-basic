7a9a688cee5e97e846bb93c63d01643b076e8141
Julita Bedkowska <julita.bedkowska@gmail.com>
2018-05-04 14:00:55 +0200

    Table printer

---
+++ students/bedkowska_julita/lesson_07_strings_datetime/table_printer.py
@@ -0,0 +1,24 @@
+def getColWidths(tableData):
+    col_widths = [0] * len(tableData)
+    for i in range(len(tableData)):
+        max = len(tableData[i][0])
+        for j in range(len(tableData[0])):
+            if len(tableData[i][j]) > max:
+                max = len(tableData[i][j])
+        col_widths[i] = max
+    return col_widths
+
+
+def printTable(tableData):
+    col_widths = getColWidths(tableData)
+    for i in range(len(tableData[0])):
+        text = ''
+        for j in range(len(tableData)):
+            text += tableData[j][i].rjust(col_widths[j]+1)
+        print(text)
+
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+print(printTable(tableData))
