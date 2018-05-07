9b77cf534dcf8fd54f0d299010b87a83aefb6755
Julita Bedkowska <julita.bedkowska@gmail.com>
2018-05-07 10:50:57 +0200

    Fix live over-indented

--- students/bedkowska_julita/lesson_07_strings_datetime/date_calculator.py
+++ students/bedkowska_julita/lesson_07_strings_datetime/date_calculator.py
@@ -7,6 +7,6 @@ minutes = int(input('Input the number of minutes: '))
 
 current_date = datetime.datetime.now()
 output_date = current_date.replace(year=current_date.year + years) \
-              + datetime.timedelta(days=days, hours=hours, minutes=minutes)
+    + datetime.timedelta(days=days, hours=hours, minutes=minutes)
 print('Result: ')
 print("{:%Y-%m-%d %H:%M}".format(output_date))
