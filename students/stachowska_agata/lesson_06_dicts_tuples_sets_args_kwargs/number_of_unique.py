a291ba69d5265fe48422bbb31adb594d543e7bef
Stachowska Agata <agata.stachowska@tieto.com>
2018-05-07 23:00:26 +0200

    added lesson 06

---
+++ students/stachowska_agata/lesson_06_dicts_tuples_sets_args_kwargs/number_of_unique.py
@@ -0,0 +1,2 @@
+numbers = {int(i) for i in input().split(' ')}
+print(len(numbers))
---
+++ students/stachowska_agata/lesson_06_dicts_tuples_sets_args_kwargs/uppercase.py
@@ -0,0 +1,10 @@
+def capitalize(lower_case_word):
+    words = lower_case_word.split(' ')
+    new_sentence = ''
+    for word in words:
+        new_sentence += chr(ord(word[0])-32) + word[1:] + ' '
+    return new_sentence
+
+
+sentence = input()
+print(capitalize(sentence))
