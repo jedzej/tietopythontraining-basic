be5eb19063e530661b0d3e03a846af51ff734626
Stachowska Agata <agata.stachowska@tieto.com>
2018-05-07 23:06:27 +0200

    fixed stickler error

--- students/stachowska_agata/lesson_06_dicts_tuples_sets_args_kwargs/uppercase.py
+++ students/stachowska_agata/lesson_06_dicts_tuples_sets_args_kwargs/uppercase.py
@@ -2,7 +2,7 @@ def capitalize(lower_case_word):
     words = lower_case_word.split(' ')
     new_sentence = ''
     for word in words:
-        new_sentence += chr(ord(word[0])-32) + word[1:] + ' '
+        new_sentence += chr(ord(word[0]) - 32) + word[1:] + ' '
     return new_sentence
 
 
