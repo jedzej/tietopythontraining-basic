ab330b5844a4a533023a45b99031fdb84e303e63
RCanDo <akasp@interia.pl>
2018-05-10 13:32:36 +0200

    06 on branch adziu/lesson_05_lists

--- students/arkadiusz_kasprzyk/lesson_05_lists/maximum_test.py
+++
@@ -1,9 +0,0 @@
-from random_matrix import *
-from maximum import *
-
-#mtx = random_matrix_int(5, 5)
-#mtx = random_matrix_int(5, 5, 99, 10)
-mtx = random_matrix_int(5, 5, 3, -3)
-
-print_table(mtx)
-print(maximum(mtx))
