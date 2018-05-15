b0128899b14b3181c2bada651b482453413cdbea
Jakielczyk Miroslaw <miroslaw.jakielczyk@tieto.com>
2018-05-15 21:16:57 +0200

    corrected mad_libs.py for lesson_09_reading_and_writing_files

--- students/jakielczyk_miroslaw/lesson_09_reading_and_writing_files/mad_libs.py
+++ students/jakielczyk_miroslaw/lesson_09_reading_and_writing_files/mad_libs.py
@@ -1,34 +1,52 @@
 file = open('file_to_be_modified.txt', 'r')
 source_text = file.read()
-print(len(source_text))
 file.close()
 
-continue_search = True
-while continue_search:
-    continue_search = False
-    if source_text.find('ADJECTIVE') != -1:
-        continue_search = True
-        print('Enter ADJECTIVE:')
-        source_text = source_text.replace('ADJECTIVE', input(), 1)
+nearest_word_position = 0
+starting_search_position = 0
 
-    if source_text.find('VERB') != -1:
-        continue_search = True
-        print('Enter VERB:')
-        source_text = source_text.replace('VERB', input(), 1)
+adjective_position = 0
+verb_position = 0
+adverb_position = 0
+noun_position = 0
 
-    if source_text.find('ADVERB') != -1:
-        continue_search = True
-        print('Enter ADVERB:')
-        source_text = source_text.replace('ADVERB', input(), 1)
+word_to_be_replaced = ''
+new_word = ''
 
-    if source_text.find('NOUN') != -1:
-        continue_search = True
-        print('Enter NOUN:')
-        source_text = source_text.replace('NOUN', input(), 1)
+while word_to_be_replaced is not None:
+    word_to_be_replaced = None
+    adjective_position = source_text.find('ADJECTIVE', starting_search_position)
+    verb_position = source_text.find('VERB', starting_search_position)
+    adverb_position = source_text.find('ADVERB', starting_search_position)
+    noun_position = source_text.find('NOUN', starting_search_position)
+
+    if adjective_position != -1:
+        nearest_word_position = adjective_position
+        word_to_be_replaced = 'ADJECTIVE'
+
+    if verb_position != -1:
+        if word_to_be_replaced is None or verb_position < nearest_word_position:
+            nearest_word_position = verb_position
+            word_to_be_replaced = 'VERB'
+
+    if adverb_position != -1:
+        if word_to_be_replaced is None or adverb_position < nearest_word_position:
+            nearest_word_position = adverb_position
+            word_to_be_replaced = 'ADVERB'
+
+    if noun_position != -1:
+        if word_to_be_replaced is None or noun_position < nearest_word_position:
+            nearest_word_position = noun_position
+            word_to_be_replaced = 'NOUN'
+
+    if word_to_be_replaced is not None:
+        print('Enter a', word_to_be_replaced.lower(), ':')
+        new_word = input()
+        source_text = source_text.replace(word_to_be_replaced, new_word, 1)
+
+    starting_search_position += len(new_word)
 
 file = open('file_after_modification.txt', 'w')
 target_text = source_text
-
 file.write(target_text)
-
 file.close()
