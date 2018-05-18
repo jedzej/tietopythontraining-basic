913f724028c2e32242bb6cef5cdf751405532335
zelazmon <hyska.monika@gmail.com>
2018-05-18 15:06:06 +0200

    new function
    add correct function for task

--- students/hyska_monika/lesson_07_manipulating_strings_datetime_formatting/ToSwapTheTwoWords.py
+++ students/hyska_monika/lesson_07_manipulating_strings_datetime_formatting/ToSwapTheTwoWords.py
@@ -3,6 +3,7 @@
 # (the second word is printed first).
 
 
+# function cut string to half and display swapped
 def change_string(text):
     half = int((len(text) / 2) + (len(text) % 2 > 0))
     second_part = text[half:]
@@ -10,6 +11,23 @@ def change_string(text):
     print(second_part + first_part)
 
 
+# correct function for this task
+def swap_2_words(text):
+    word1, word2 = text.split(' ')
+    changed_text = word2 + ' ' + word1
+    print(changed_text)
+
+
+my_string = 'Ani Aladddddddd'
+my_string2 = 'Ani5 Ala'
+my_string3 = 'A sdfdfdsfsd'
+
+swap_2_words(my_string)
+swap_2_words(my_string2)
+swap_2_words(my_string3)
+
+# -----------------
+print('')
 my_string = 'AniAla'
 my_string2 = 'Ani5Ala'
 my_string3 = 'A'
