8676e18f651e58adef6ed33f654d3f96f41f19d8
Jakielczyk Miroslaw <miroslaw.jakielczyk@tieto.com>
2018-05-18 13:15:43 +0200

    corrected files according to @kuszcjan remarks

--- students/jakielczyk_miroslaw/lesson_09_reading_and_writing_files/mad_libs.py
+++ students/jakielczyk_miroslaw/lesson_09_reading_and_writing_files/mad_libs.py
@@ -1,52 +1,23 @@
-file = open('file_to_be_modified.txt', 'r')
-source_text = file.read()
-file.close()
+source_file = 'file_to_be_modified.txt'
+target_file = 'file_after_modification.txt'
+words_to_be_replaced = ['ADJECTIVE', 'ADVERB', 'VERB', 'NOUN']
 
-nearest_word_position = 0
-starting_search_position = 0
+with open(target_file, 'w') as outfile:
+    with open(source_file, 'r', encoding='utf-8') as infile:
+        for line in infile:
+            target_line = []
+            for word in line.split():
+                new_word = word
+                for replace_word in words_to_be_replaced:
+                    if word.find(replace_word) != -1:
+                        print('Enter ', replace_word)
+                        new_word = input()
+                        new_word = word.replace(replace_word, new_word)
+                        break
+                target_line.append(new_word)
+            outfile.write(' '.join(target_line)+'\n')
 
-adjective_position = 0
-verb_position = 0
-adverb_position = 0
-noun_position = 0
 
-word_to_be_replaced = ''
-new_word = ''
 
-while word_to_be_replaced is not None:
-    word_to_be_replaced = None
-    adjective_position = source_text.find('ADJECTIVE', starting_search_position)
-    verb_position = source_text.find('VERB', starting_search_position)
-    adverb_position = source_text.find('ADVERB', starting_search_position)
-    noun_position = source_text.find('NOUN', starting_search_position)
 
-    if adjective_position != -1:
-        nearest_word_position = adjective_position
-        word_to_be_replaced = 'ADJECTIVE'
 
-    if verb_position != -1:
-        if word_to_be_replaced is None or verb_position < nearest_word_position:
-            nearest_word_position = verb_position
-            word_to_be_replaced = 'VERB'
-
-    if adverb_position != -1:
-        if word_to_be_replaced is None or adverb_position < nearest_word_position:
-            nearest_word_position = adverb_position
-            word_to_be_replaced = 'ADVERB'
-
-    if noun_position != -1:
-        if word_to_be_replaced is None or noun_position < nearest_word_position:
-            nearest_word_position = noun_position
-            word_to_be_replaced = 'NOUN'
-
-    if word_to_be_replaced is not None:
-        print('Enter a', word_to_be_replaced.lower(), ':')
-        new_word = input()
-        source_text = source_text.replace(word_to_be_replaced, new_word, 1)
-
-    starting_search_position += len(new_word)
-
-file = open('file_after_modification.txt', 'w')
-target_text = source_text
-file.write(target_text)
-file.close()
--- students/jakielczyk_miroslaw/lesson_09_reading_and_writing_files/regex_search.py
+++ students/jakielczyk_miroslaw/lesson_09_reading_and_writing_files/regex_search.py
@@ -7,16 +7,18 @@ def collect_arguments(folder_path, regex_expression):
     folder_content = os.listdir(folder_path)
     list_of_text_files = []
     for file in folder_content:
-        if file[-4:] == '.txt':
+        if file.endswith(".txt"):
             list_of_text_files.append(file)
 
     for text_file in list_of_text_files:
         print('\n', text_file)
         file = open(os.path.join(folder_path, text_file), 'r')
-        list_of_text_lines = file.readlines()
-        for each_text_line in list_of_text_lines:
-            print(regex_search.findall(each_text_line))
-        file.close()
+        try:
+            list_of_text_lines = file.readlines()
+            for each_text_line in list_of_text_lines:
+                print(regex_search.findall(each_text_line))
+        finally:
+            file.close()
 
 
 def main():
