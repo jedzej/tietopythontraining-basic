f93e43072104148ee030c73d45569d798a30ed78
zelazmon <hyska.monika@gmail.com>
2018-05-18 12:10:46 +0200

    change variable name
    change variable name

--- students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/AccessRights.py
+++ students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/AccessRights.py
@@ -3,14 +3,14 @@ from functions_lesson06 import list_elements_str, change_to_dict
 
 
 def access_rights(inventory):
-    rights = {'read': 'r', 'write': 'w', 'execute': 'x'}
+    rights_definition = {'read': 'r', 'write': 'w', 'execute': 'x'}
     n = int((input('How many access rights do you want check?:')))
     for i in range(n):
         command = str((input("put command:")))
         command = command.split(' ')
         try:
             right = command[0]
-            right = rights[right]
+            right = rights_definition[right]
             file_name = command[1]
             file_rights = inventory[file_name]
             if right in file_rights:
@@ -25,7 +25,8 @@ print("For size list put how many files do you have."
       "\nFor one element of list put file name and access rights,"
       " separate using space."
       "\nEg. kot.j x w r")
-access_rights(change_to_dict(list_elements_str()))
+rights = change_to_dict(list_elements_str())
+access_rights(rights)
 
 # inventory = {'kot.j' : ['r', 'w',  'x'],
 #             'pies.l' : ['r'], 'mysz' : ['r', 'x']}
