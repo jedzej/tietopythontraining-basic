7fe9ce5b642355560ae5a346fde0afaa03a6d114
zelazmon <hyska.monika@gmail.com>
2018-05-18 12:01:48 +0200

    add_ new _functiion
    add new function work when dictionary doesn't have lists inside

--- students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/CountriesAndCities.py
+++ students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/CountriesAndCities.py
@@ -1,9 +1,11 @@
 # Function print country name for city
-from functions_lesson06 import list_elements_str, change_to_dict
+from functions_lesson06 import list_elements_str, change_to_dict,\
+    change_dict_without_list
 
 
+# search country if dictionary have lists inside
 def countries_and_cities(inventory):
-    n = int((input("How cities do you want check?: ")))
+    n = int((input("How many cities do you want check?: ")))
     for i in range(n):
         city = str((input("put city:")))
         country = None
@@ -17,12 +19,26 @@ def countries_and_cities(inventory):
             print("country: ", country)
 
 
+# search country if dictionary doesn't have list inside
+def search_country_for_city(inventory):
+    n = int((input("How many cities do you want check?: ")))
+    for i in range(n):
+        city = str((input("put city:")))
+        country = inventory.get(city)
+        if country is None:
+            print("Missing city on list.")
+        else:
+            print("country: ", country)
+
+
 print("For one element of list put country and cities, separate using space."
       "\nEg. Poland Warsszawa Wroclawa Krakow")
-countries_and_cities(change_to_dict(list_elements_str()))
+countries_and_cities_list = change_to_dict(list_elements_str())
+# countries_and_cities(countries_and_cities_list)
+search_country_for_city(change_dict_without_list(countries_and_cities_list))
 """
 inventory = {'USA' : ['Dallas', 'Chicago',  'Atlanta'],
              'Polska' : ['Warszawa', 'Krakow'],
              'Niemcy' : ['Berlin', 'Dresno']}
-countries_and_cities(inventory)
+search_country_for_city(change_dict_without_list(inventory))
 """
