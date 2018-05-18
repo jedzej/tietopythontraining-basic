cef9f9c50454dce1abe658f153cfbadf990527d0
zelazmon <hyska.monika@gmail.com>
2018-05-18 12:08:33 +0200

    variable name
    change variable name to retutn

--- students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/Uppercase.py
+++ students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/Uppercase.py
@@ -4,13 +4,14 @@
 
 def capitalize(lower_case_word):
     lst = [word[0].upper() + word[1:] for word in lower_case_word.split()]
-    lower_case_word = " ".join(lst)
-    print(lower_case_word)
-    return lower_case_word
+    capital_case_word = " ".join(lst)
+    print(capital_case_word)
+    return capital_case_word
 
 
 text = str((input("Put string - use small letter: ")))
 capitalize(text)
 
+print("")
 s = "ala's ma kota UK. kk"
 capitalize(s)
