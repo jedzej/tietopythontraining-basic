432fdbb5c3e8a94f4eb254ed8b149dff99e96188
zelazmon <hyska.monika@gmail.com>
2018-05-18 14:15:06 +0200

    add second way
    add new funcition - second way

--- students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/GuessTheNumber.py
+++ students/hyska_monika/lesson_06_dicts_tuples_sets_args_kwargs/GuessTheNumber.py
@@ -5,7 +5,7 @@ from functions_lesson06 import create_dict_to_n, find_max
 
 def number_check(asked_numbers):
     while True:
-        numbers = (input("put numbers with space: "))
+        numbers = (input("put numbers with spaces: "))
         if numbers == "help":
             asked_numbers = {key: val for key, val in asked_numbers.items()
                              if val != 0}
@@ -25,5 +25,26 @@ def number_check(asked_numbers):
     find_max(asked_numbers)
 
 
-n = int((input("range for number:")))
-number_check(create_dict_to_n(n))
+# second way
+def number_check2(n):
+    valid = set(range(1, n))
+    while True:
+        numbers = (input("Put numbers with spaces (or HELP): "))
+        if numbers == 'HELP':
+            print("\nSecret number is on list:")
+            print(' '.join(map(str, valid)))
+            break
+        else:
+            row = set(map(int, numbers.split(' ')))
+            command = input('YES/NO: ')
+            if command == 'YES':
+                valid &= row
+            elif command == 'NO':
+                valid -= row
+            else:
+                raise ValueError('WRONG ANSWER')
+
+
+n = int((input("Range for number: ")))
+# number_check(create_dict_to_n(n))
+number_check2(n)
