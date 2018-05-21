b2f3b98a7b2b68211c0050cbd7335800f7fc5786
zelazmon <hyska.monika@gmail.com>
2018-05-21 15:10:15 +0200

    assertAlmostEqual
    assertEqual -> assertAlmostEqual

--- students/hyska_monika/lesson_04_unit_testing/distance_test.py
+++ students/hyska_monika/lesson_04_unit_testing/distance_test.py
@@ -41,33 +41,33 @@ class DistanceTest(unittest.TestCase):
 
     # Tests it on corner cases:
     def test_vaule_zero(self):
-        self.assertEqual(distance.distance(2, 1, 2, 1), 0.0)    # Zero length
+        self.assertAlmostEqual(distance.distance(2, 1, 2, 1), 0.0)    # Zero length
 
     def test_vaule_negative(self):
-        self.assertEqual(distance.distance(-1, -1, -3, -5),
+        self.assertAlmostEqual(distance.distance(-1, -1, -3, -5),
                          4.47214)  # Negative coordinates
 
     def test_vaule_vertical(self):
-        self.assertEqual(distance.distance(2, 1, 2, 7),
+        self.assertAlmostEqual(distance.distance(2, 1, 2, 7),
                          6.0)  # Only vertical distance
 
     def test_vaule_horizontaly(self):
-        self.assertEqual(distance.distance(5, 1, 10, 1),
+        self.assertAlmostEqual(distance.distance(5, 1, 10, 1),
                          5.0)  # Only horizontal distance
 
     # Typical conditions (difference on both coordinates)
     def test_vaule_1(self):
-        self.assertEqual(distance.distance(3, -2, -1, 7), 9.84886)
+        self.assertAlmostEqual(distance.distance(3, -2, -1, 7), 9.84886)
 
     def test_vaule_2(self):
-        self.assertEqual(distance.distance(-10, 2, 5, 12), 18.02776)
+        self.assertAlmostEqual(distance.distance(-10, 2, 5, 12), 18.02776)
 
     def test_vaule_3(self):
-        self.assertEqual(distance.distance(1, -5, -10, -12), 13.0384)
+        self.assertAlmostEqual(distance.distance(1, -5, -10, -12), 13.0384)
 
     # Test that the order of points does not matter
     def test_order(self):
-        self.assertEqual(distance.distance(5, 1, 10, 2),
+        self.assertAlmostEqual(distance.distance(5, 1, 10, 2),
                          distance.distance(10, 2, 5, 1))
 
 
