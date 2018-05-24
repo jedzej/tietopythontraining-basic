450d46fd736b82f4a9a8169cf05efe7e6a59d759
zelazmon <hyska.monika@gmail.com>
2018-05-24 09:45:36 +0200

    add function
    I added new function with optiion add date(without month)

--- students/hyska_monika/lesson_07_manipulating_strings_datetime_formatting/DateCalculator.py
+++ students/hyska_monika/lesson_07_manipulating_strings_datetime_formatting/DateCalculator.py
@@ -4,6 +4,7 @@
 import datetime
 
 
+# function to add years, months, days, hours, minutes to date
 def add_to_date(date, years, months, days, hours, minutes):
     new_date = date + datetime.timedelta(days=days,
                                          hours=hours,
@@ -15,10 +16,21 @@ def add_to_date(date, years, months, days, hours, minutes):
     return new_date2
 
 
+# function to add years, days, hours, minutes to date
+def add_to_date2(date, years, days, hours, minutes):
+    new_date = date + datetime.timedelta(days=days,
+                                         hours=hours,
+                                         minutes=minutes)
+    new_date2 = new_date.replace(new_date.year + years)
+    print('Changed date: ', new_date2.strftime("%Y-%m-%d %H:%M"))
+    return new_date2
+
+
 now = datetime.datetime.now()
 print('Actual date and time: ', now.strftime("%Y-%m-%d %H:%M"))
 add_to_date(now, 2, 4, 3, 5, 30)
 add_to_date(now, 5, 15, 35, 28, 58)
+add_to_date2(now, 2, 20, 5, 30)
 
 test_date = datetime.datetime(1999, 11, 25, 23, 58)
 print('\nCheck for date and time: ', test_date.strftime("%Y-%m-%d %H:%M"))
@@ -27,3 +39,5 @@ add_to_date(test_date, 2, 4, 3, 5, 30)
 add_to_date(test_date, 5, 15, 35, 25, 58)
 add_to_date(test_date, 5, 3, 7, 3, 4)
 add_to_date(test_date, 5, 389, 455, 15897, 65)
+add_to_date2(test_date, 2, 50, 5, 30)
+add_to_date2(test_date, 20, 389, 15897, 65)
