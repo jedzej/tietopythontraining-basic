bfdf91a3fa11e63fce4f61a963f883cdbaed4df6
gubryann <anna.piatkowska@tieto.com>
2018-05-29 11:15:22 +0200

    remove unused variable

--- students/piatkowska_anna/lesson_07_strings/date_printer.py
+++ students/piatkowska_anna/lesson_07_strings/date_printer.py
@@ -7,9 +7,6 @@ from datetime import datetime
 
 def current_date():
     now = datetime.now()
-    months = {1: "January", 2: "February", 3: "Mars", 4: "April", 5: "May",
-              6: "June", 7: "July", 8: "August", 9: "September",
-              10: "October", 11: "November", 12: "December"}
     return '{:%A %Y %B %d %H:%M (week number:%U)}'.format(now)
 
 
