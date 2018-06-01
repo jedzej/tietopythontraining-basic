76d3cd8663b8ad508ecb643db5026a1f810b4485
zelazmon <hyska.monika@gmail.com>
2018-06-01 09:36:24 +0200

    separation of subfunction

--- students/hyska_monika/lesson_10_organizing_files/deleting_unneeded_files.py
+++ students/hyska_monika/lesson_10_organizing_files/deleting_unneeded_files.py
@@ -1,5 +1,5 @@
 """
-Program  walks through a folder tree and searches for
+Program  walks in to a bar tree and searches for
 exceptionally large files or folders, eg. size > 8 mb
 Program delete found files and folders
 """
@@ -12,6 +12,15 @@ def search_files_folders_bigger_size(path, size):
     size_in_bytes = size * 1048576
     files_list = []
     folders_list = []
+    find_files_folder_bigger_size(files_list, folders_list, path, size_in_bytes)
+    print('Folders bigger then', size, 'mb:')
+    print('\n'.join(map(str, folders_list)))
+    print('Files bigger then', size, 'mb:')
+    print('\n'.join(map(str, files_list)))
+    return folders_list, files_list
+
+
+def find_files_folder_bigger_size(files_list, folders_list, path, size_in_bytes):
     for folder_name, sub_folders, file_names in os.walk(path):
         folder_size = 0
         for file_name in file_names:
@@ -21,11 +30,6 @@ def search_files_folders_bigger_size(path, size):
             folder_size += os.path.getsize(file_name_with_path)
         if folder_size > size_in_bytes:
             folders_list.append(os.path.abspath(folder_name))
-    print('Folders bigger then', size, 'mb:')
-    print('\n'.join(map(str, folders_list)))
-    print('Files bigger then', size, 'mb:')
-    print('\n'.join(map(str, files_list)))
-    return folders_list, files_list
 
 
 # function delete folders and file
@@ -38,7 +42,7 @@ def delete_to_big_folders_files(folders_list, files_list):
             os.unlink(file)
 
 
-search_files_folders_bigger_size('.\\folder', 8)
+search_files_folders_bigger_size('.\\folder', 0.05)
 
 # --- you can check it after run selective_copy ---
 # my_folders, my_files = search_files_folders_bigger_size('.\\new_folder', 8)
