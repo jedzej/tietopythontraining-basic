b16b289024925b740e20cafd9dff4c43bfa6329a
Chylak Malgorzata <malgorzata.chylak@tieto.com>
2018-06-06 13:41:37 +0200

    Lesson 07 update

--- students/chylak_malgorzata/lesson_07_manipulating_strings_+_datetime_formatting/date_calculator.py
+++ students/chylak_malgorzata/lesson_07_manipulating_strings_+_datetime_formatting/date_calculator.py
@@ -6,5 +6,5 @@ hour = int(input('Hours: '))
 minute = int(input('Minutes: '))
 
 current_date = datetime.now() + timedelta(weeks=year * 52, days=day,
-             hours=hour, minutes=minute)
+                                          hours=hour, minutes=minute)
 print('The new date is: ' + current_date.strftime("%d-%m-%Y %H:%M:%S"))
