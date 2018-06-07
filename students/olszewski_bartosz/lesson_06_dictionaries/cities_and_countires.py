c0ce8afc42f827cdafdd017ec7ac1bbb0cedb234
olszebar <bartosz.olszewski@tieto.com>
2018-06-07 11:24:35 +0200

    Update cities_and_countires.py

--- students/olszewski_bartosz/lesson_06_dictionaries/cities_and_countires.py
+++ students/olszewski_bartosz/lesson_06_dictionaries/cities_and_countires.py
@@ -1,8 +1,10 @@
 number_of_countries = int(input())
 cities = dict()
+ACTION = 0
+ACTION_2 = 1
 for city in range(number_of_countries):
     entry = input().split(' ')
-    cities[entry[0]] = entry[1:]
+    cities[entry[ACTION]] = entry[ACTION_2:]
 
 number_of_checks = int(input())
 for checks in range(number_of_checks):
