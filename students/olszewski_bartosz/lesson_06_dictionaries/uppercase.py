3b14edd5be6afa7194d91b399845516d7ad71c8a
olszebar <bartosz.olszewski@tieto.com>
2018-06-07 11:46:07 +0200

    Update uppercase.py

--- students/olszewski_bartosz/lesson_06_dictionaries/uppercase.py
+++ students/olszewski_bartosz/lesson_06_dictionaries/uppercase.py
@@ -2,15 +2,10 @@ a = input()
 
 
 def capitalize(sentence):
-    for i in range(len(sentence)):
-        if i == 0:
-            b = ord(sentence[i]) - 32
-            print(chr(b), end='')
-        elif sentence[i - 1] == ' ':
-            b = ord(sentence[i]) - 32
-            print(chr(b), end='')
-        else:
-            print(sentence[i], end='')
+    capitalized_words = []
+    for word in sentence.split():
+        capitalized_words.append(word.title())
+    print(" ".join(capitalized_words))
 
 
 capitalize(a)
