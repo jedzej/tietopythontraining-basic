e16134e990a2505be49135bd58fc3a346f2514f3
Halas Maciej <maciej.halas@tieto.com>
2018-06-07 10:48:10 +0200

    lesson_06

---
+++ students/access_rights.py
@@ -0,0 +1,16 @@
+operations = {'read': 'R', 'write': 'W', 'execute': 'X'}
+files = {}
+
+for i in range(int(input())):
+    name, *rights = input().split()
+    files[name] = rights
+
+
+for test in range(int(input())):
+    operation, name = (i for i in input().split(' '))
+    if operations.get(operation) in files.get(name):
+        print('OK')
+    else:
+        print('Access denied')
+
+        
