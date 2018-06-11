65037c89b92441bd78719e24e966d4c8dbb18c93
Szymon Mielczarek <szymon.mielczarek@tieto.com>
2018-06-11 19:30:30 +0200

    Add buggy coin toss program
    To debug and fix.

---
+++ students/mielczarek_szymon/lesson_12_debugging/coin_toss_debugging.py
@@ -0,0 +1,17 @@
+#! python3
+
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+toss = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guesss = input()
+    if toss == guess:
+       print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
