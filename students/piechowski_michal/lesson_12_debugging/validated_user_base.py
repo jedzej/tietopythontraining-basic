a49b400aa70862249a6bebff59936ea307853d73
Michal Piechowski <michal.a.piechowski@tieto.com>
2018-06-22 12:41:50 +0200

    Fix code according to review comments

--- students/piechowski_michal/lesson_12_debugging/validated_user_base.py
+++ students/piechowski_michal/lesson_12_debugging/validated_user_base.py
@@ -2,6 +2,7 @@
 
 import argparse
 import csv
+import logging
 import sys
 
 from students.piechowski_michal.lesson_08_regular_expressions.email_validator\
@@ -13,26 +14,19 @@ from students.piechowski_michal.lesson_08_regular_expressions.postal_code_valida
 from students.piechowski_michal.lesson_08_regular_expressions.strong_password_detection\
     import is_strong as is_strong_password
 
-
-verbosity = ""
-
-
-def my_print(tag, text):
-    if verbosity == "disabled":
-        return
-    if tag == "warning" or verbosity == "info":
-        print(text)
+logging.basicConfig(level=logging.INFO,
+                    format=' %(asctime)s - %(levelname)s - %(message)s')
 
 
 def combined_validator(email, password, phone_number, postal_code):
-    my_print("info", "Verifying user data")
+    logging.info( "Verifying user data")
 
     if is_valid_email(email) and is_strong_password(password) \
             and is_valid_phone_number(phone_number) \
             and is_valid_postal_code(postal_code):
-        my_print("info", "Verification passed")
+        logging.info( "Verification passed")
     else:
-        my_print("warning", "Verification failed")
+        logging.warning( "Verification failed")
         raise Exception("One of arguments didn't pass verification.")
 
 
@@ -45,7 +39,13 @@ def check_arg(args):
                         default='info')
 
     results = parser.parse_args(args)
-    return results.log
+
+    if results.log == 'disabled':
+        return logging.CRITICAL
+    elif results.log == 'warning':
+        return logging.WARNING
+    else:
+        return logging.INFO
 
 
 def save_user_data(email, password, phone_number, postal_code):
@@ -54,18 +54,18 @@ def save_user_data(email, password, phone_number, postal_code):
     with open(csv_file, 'a') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow([email, password, phone_number, postal_code])
-        my_print("warning", "User data has been written")
+        logging.warning( "User data has been written")
 
     with open(csv_file, 'r') as file:
         reader = csv.reader(file, delimiter=',')
-        my_print("info", "Currently we have following rows:")
+        logging.info( "Currently we have following rows:")
         for row in reader:
-            my_print("info", ' '.join(row))
+            logging.info( ' '.join(row))
 
 
 def main():
-    global verbosity
     verbosity = check_arg(sys.argv[1:])
+    logging.getLogger().setLevel(verbosity)
 
     email = input("Input email (e.g. some@email.com): ")
     password = input("Input password (e.g. some_Pass8): ")
@@ -75,7 +75,7 @@ def main():
     try:
         combined_validator(email, password, phone_number, postal_code)
     except Exception:
-        my_print("warning", "Exception arised, not saving the user data")
+        logging.warning( "Exception arised, not saving the user data")
         return
 
     save_user_data(email, password, phone_number, postal_code)
