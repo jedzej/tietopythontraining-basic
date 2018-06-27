41b32e01b67e71a0804cd8d893d979b9109336b3
jedzej <jedrzej.wiewiora@gmail.com>
2018-06-27 19:56:57 +0200

    Update authorization.py

--- course/lesson_14_decorators/authorization.py
+++ course/lesson_14_decorators/authorization.py
@@ -4,6 +4,7 @@ def has_access():
   '''
   let's pretend this function calls some authorization services
   returns True if access is granted and False if not
+  access is granted every 3rd time
   '''
   global access_count
   access_count += 1
