35bdf866472a952ab8b9c7c8d0a73858cc9bff63
dychomon <monika.dychowicz@tieto.com>
2018-06-29 15:24:48 +0200

    lesson 7 the rest

---
+++ students/dychowicz_monika/lesson_07/ReplaceTheSubstring.py
@@ -0,0 +1,6 @@
+def main():
+    print(input().replace("1", "one"))
+
+
+if __name__ == '__main__':
+    main()
---
+++ students/dychowicz_monika/lesson_07/deltaTimeCalculator.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def print_delta_time(future_date):
+    today_date = datetime.now()
+    print(today_date)
+    delta = future_date - today_date
+    print('DELTA TIME: {}'.format(delta))
+
+
+def main():
+    date_from_future = datetime(2033, 12, 23)
+    print_delta_time(date_from_future)
+
+
+if __name__ == '__main__':
+    main() 
