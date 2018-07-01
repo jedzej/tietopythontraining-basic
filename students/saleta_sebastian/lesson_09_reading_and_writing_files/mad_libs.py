8df564314286fbf6aad806df8919df4c60489828
saletseb <sebastian.saleta@tieto.com>
2018-07-01 11:59:08 +0200

    New solution to replace words in .txt file

--- students/saleta_sebastian/lesson_09_reading_and_writing_files/mad_libs.py
+++ students/saleta_sebastian/lesson_09_reading_and_writing_files/mad_libs.py
@@ -1,15 +1,31 @@
+import re
+
+
+WORDS = ['ADJECTIVE', 'NOUN', 'VERB', 'NOUN']
+
+
 def mad_libs(adjective, noun, verb, noun2):
+    mad_words = [adjective, noun, verb, noun2]
+    story = open('story.txt', 'r')
+    panda_story = story.read()
+    print(len(WORDS))
+    for i in range(len(WORDS)):
+        story_editor = re.sub(r"\b{}\b".format(WORDS[i]), mad_words[i],
+                              panda_story, count=1)
+        panda_story = story_editor
+    return story_editor
+
+
+def create_story_file():
     file_story = open('story.txt', 'w')
-    story = ('The {} panda walked to the {} and then {}. A nearby {} '
-             'was unaffected by these events.'
-             .format(adjective, noun, verb, noun2))
+    story = ('The ADJECTIVE panda walked to the NOUN and'
+             ' then VERB. A nearby NOUN was unaffected by these events.')
     file_story.write(story)
     file_story.close()
 
-    return story
-
 
 def main():
+    create_story_file()
     user_adjective = input('Enter an adjective: ')
     user_noun = input('Enter a noun: ')
     user_verb = input('Enter a verb: ')
