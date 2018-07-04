836d60e1a83cf806e1765b609178b6e24aeea5c3
Wojciech Serek <wojciech.serek@tieto.com>
2018-07-04 11:24:49 +0200

    Fix filling_gaps

--- students/serek_wojciech/lesson_10_organizing_files/filling_gaps.py
+++ students/serek_wojciech/lesson_10_organizing_files/filling_gaps.py
@@ -13,11 +13,11 @@ TEST_FOLDER = 'test_tree_gaps'
 def main():
     """Main function"""
     last_file_num = 0
+    next_file_number = 0
     for file in os.listdir(TEST_FOLDER):
         if re.match(r'{}\d{{3}}{}'.format(FILE_PREFIX, FILE_EXT), file):
             regex = re.compile(r'\d+')
             current_file_number = int(regex.search(file).group(0))
-            next_file_number = last_file_num + 1
             if current_file_number != next_file_number:
                 shutil.move(os.path.join(TEST_FOLDER, file),
                             os.path.join(TEST_FOLDER, '{}{:03d}{}'.format(
@@ -25,6 +25,7 @@ def main():
                 last_file_num = next_file_number
             else:
                 last_file_num = current_file_number
+            next_file_number = last_file_num + 1
 
 
 if __name__ == '__main__':
