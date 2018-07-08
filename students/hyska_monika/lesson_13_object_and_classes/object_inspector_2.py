e8c9f1023b69ed2c7595f8bf88b49dc0482c0225
zelazmon <hyska.monika@gmail.com>
2018-07-08 20:48:48 +0200

    lesson_13_Monika_Hyska

---
+++ students/hyska_monika/lesson_13_object_and_classes/object_inspector_2.py
@@ -0,0 +1,40 @@
+"""
+Object inspector 2 - write a function that for a given object
+returns dictionary with names and values of all object's
+attributes that are instances of string, integer or float.
+"""
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        """ Create a new point at the origin """
+        self.x = x
+        self.y = y
+
+
+def object_inspector2(instance, keys):
+    instance_all = instance.__dict__
+    selected_keys = {key: instance_all[key] for key in keys
+                     if (isinstance(instance_all[key], int) or
+                     isinstance(instance_all[key], str) or
+                     isinstance(instance_all[key], float))}
+    return(selected_keys)
+
+
+def main():
+    ainspector = Point()
+    ainspector.g = 7
+    ainspector.x = 12
+    ainspector.b = [77, 88]
+    ainspector.c = 4
+    ainspector.y = {'c':77, 'g':88}
+    ainspector.s = "sssss"
+    ainspector.p = 4.564744
+    ainspector.q = 5
+    ainspector.m = 4.564744
+
+    a = ['b', 'x', 'y', 'p', 's']
+    print(object_inspector2(ainspector, a))
+
+if __name__ == "__main__":
+    main()
---
+++ students/hyska_monika/lesson_13_object_and_classes/selective_shallow_compare.py
@@ -0,0 +1,50 @@
+"""
+Selective shallow compare - write a function that for given 2 objects
+and list of attribute names checks if objects' attributes are equal.
+"""
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        """ Create a new point at the origin """
+        self.x = x
+        self.y = y
+
+
+def compere_objects(instance1, instance2, keys):
+    instance1_all = instance1.__dict__
+    instance2_all = instance2.__dict__
+    compare_result = {key: (instance1_all[key] == instance2_all[key])
+                      for key in keys}
+    return(compare_result)
+
+
+def main():
+    ainspector = Point()
+    ainspector.g = 7
+    ainspector.x = 12
+    ainspector.b = [77, 88]
+    ainspector.c = 4
+    ainspector.y = {'c':77, 'g':88}
+    ainspector.s = "sssss"
+    ainspector.p = 4.564744
+    ainspector.q = 5
+    ainspector.m = 4.564744
+
+    binspector = Point()
+    binspector.g = 7
+    binspector.x = 5
+    binspector.b = [22, 88]
+    binspector.c = 4
+    binspector.y = {'c':77, 'g':88}
+    binspector.s = "ssassss"
+    binspector.p = 4.564744
+    binspector.q = 5
+    ainspector.m = 4.564744
+
+    attributes = ['b', 'x', 'y', 'p', 's']
+    print(compere_objects(ainspector, binspector, attributes))
+
+
+if __name__ == "__main__":
+    main()
