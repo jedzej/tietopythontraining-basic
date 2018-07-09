245a100ed6324b9f8dfeae2780883fe272ddf5f7
Aleksander Kulon <olo3@poczta.onet.pl>
2018-07-09 22:32:07 +0200

    fixes
    Signed-off-by: Aleksander Kulon <olo3@poczta.onet.pl>

--- students/kulon_aleksander/lesson_11/filling_in_the_gaps.py
+++ students/kulon_aleksander/lesson_11/filling_in_the_gaps.py
@@ -30,16 +30,15 @@ def main():
         for file_name in file_names:
             result = re.search(prefix_pattern, file_name)
             if result is not None:
-                sorted_numbers.append(result.group(0).replace(results.pattern, ''))
+                sorted_numbers.append(result.group(0).replace(results.pattern,
+                                                              ''))
 
     sorted_numbers = sorted(sorted_numbers)
 
     for i in range(int(sorted_numbers[0]), int(sorted_numbers[-1]) - 1):
-        current = str(sorted_numbers[i-1]).zfill(3)
-        next = str(int(sorted_numbers[i-1]) + 1).zfill(3)
-        later = str(int(sorted_numbers[i-1]) + 2).zfill(3)
+        next = str(int(sorted_numbers[i - 1]) + 1).zfill(3)
+        later = str(int(sorted_numbers[i - 1]) + 2).zfill(3)
 
-        current_file = path + results.pattern + current + '.txt'
         next_file = path + results.pattern + next + '.txt'
         later_file = path + results.pattern + later + '.txt'
         if not os.path.isfile(next):
