0eb37b7558f57c4d204a81f74ea46347cf7c44fd
Natalia Jarosz <natalia.jarosz@tieto.com>
2018-07-19 18:29:12 +0200

    fixes once again

--- students/jarosz_natalia/lesson_10/filling_in_the_gaps.py
+++ students/jarosz_natalia/lesson_10/filling_in_the_gaps.py
@@ -19,32 +19,20 @@ def main():
     sorted_numbers = sorted(sorted_numbers)
 
     for i in range(int(sorted_numbers[0]), int(sorted_numbers[-1]) - 1):
-        #print("size ", len(sorted_numbers))
         if i == len(sorted_numbers):
-            #print("last element, break")
             break
 
         next_number = str(i + 1).zfill(3)
-        #print("i: ", i)
 
         for j in range(i, int(sorted_numbers[-1])):
             later_number = str(j + 1).zfill(3)
             later_file = PATH + PATTERN + later_number + '.txt'
             if os.path.isfile(later_file):
-                #print("later file: ", later_file)
                 break
             else:
-                #print("not found: ", later_file)
-
-        #print("next_number ", next_number)
-        #print("later_number ", later_number)
-
-        next_file = PATH + PATTERN + next_number + '.txt'
+                next_file = PATH + PATTERN + next_number + '.txt'
         later_file = PATH + PATTERN + later_number + '.txt'
-        print("next_file ", next_file)
-        print("later_file ", later_file)
         if not os.path.isfile(next_number):
-            print("replace ", later_file, " with ", next_file)
             shutil.move(later_file, next_file)
 
 
