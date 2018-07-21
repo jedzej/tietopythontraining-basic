39acd9675c73d74600ea561996261636553d97f5
Grzegorz Bajorski <Grzegorz.Bajorski@tieto.com>
2018-07-21 19:06:15 +0200

    Lesson 11
    Signed-off-by: Grzegorz Bajorski <Grzegorz.Bajorski@tieto.com>

---
+++ students/grzegorz_bajorski/lesson_11_argparse/regex_search.py
@@ -0,0 +1,22 @@
+import re
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Fillings the gaps program')
+
+parser.add_argument('-d',
+                help="regex expression",
+                regex='regex',
+                required=True)
+
+args = parser.parse_args()
+
+regex = args.regex
+search_exp = re.compile(regex)
+
+for files in os.listdir('.'):
+    if files.endswith(".txt"):
+        opened_file = open(files, 'r')
+        for line in opened_file:
+            if search_exp.match(line):
+                print("Found in " + files + "\n")
---
+++ students/grzegorz_bajorski/lesson_11_argparse/selective_copy.py
@@ -0,0 +1,26 @@
+import shutil 
+import os
+import argparse
+
+
+dest_dir = "copy_folder/"
+os.mkdir(dest_dir)
+
+parser = argparse.ArgumentParser(description='Selective Copy program')
+
+parser.add_argument('-d',
+		help="Destination directory.",
+		dest='dest_dir',
+		required=True)
+
+args = parser.parse_args()
+
+dest_dir = args.dest_dir
+
+for filename in os.listdir():
+    if filename.endswith('.pdf'):
+        print(filename)
+        shutil.copy(filename, dest_dir)
+    if filename.endswith('.txt'):
+        print(filename)
+        shutil.copy(filename, dest_dir)
