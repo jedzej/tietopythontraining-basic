546d670ac422a20c92890ccd275022fd41923559
Chylak Malgorzata <malgorzata.chylak@tieto.com>
2018-07-23 12:45:20 +0200

    lesson_09_correction

---
+++ students/chylak_malgorzata/lesson_09_reading_and_writing_files/regex_search.py
@@ -0,0 +1,19 @@
+import re
+import os
+
+lsdir = [name for name in os.listdir('.') if 'txt' in name]
+print(lsdir)
+
+
+def main():
+    regex = re.compile(r'{}'.format(input("Enter a regex: ")))
+    for filename in os.listdir('.'):
+        if filename.endswith(".txt"):
+            with open(filename) as file:
+                for line in file.readlines():
+                    if regex.match(line):
+                        print(line)
+
+
+if __name__ == "__main__":
+        main()
