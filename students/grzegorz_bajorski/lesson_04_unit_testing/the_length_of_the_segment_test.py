748ba798a3a28c7cafb152a2989cc388857df99b
Grzegorz Bajorski <Grzegorz.Bajorski@tieto.com>
2018-07-23 13:52:23 +0200

    Fixes
    Signed-off-by: Grzegorz Bajorski <Grzegorz.Bajorski@tieto.com>

--- students/grzegorz_bajorski/lesson_04_unit_testing/the_length_of_the_segment_test.py
+++ students/grzegorz_bajorski/lesson_04_unit_testing/the_length_of_the_segment_test.py
@@ -2,28 +2,30 @@ import pytest
 from the_length_of_the_segment import distance
 
 
-class MyTest(pytest.TestCase):
+class MyTest(pytest):
 
     def test_1(self):
-        self.assertRaises(TypeError, distance, None, 1, 2, 3)
+        with pytest.raises(TypeError):
+            distance(None, 1,  2, 3)
 
     def test_2(self):
-        self.assertRaises(ValueError, distance, 'aoeu', 1, 2, 3)
+        with pytest.raises(ValueError):
+            distance('aoeu', 1, 2, 3)
 
     def test_3(self):
-        self.assertEqual(distance(1, 1, 1, 1), 0)
+        assert distance(1, 1, 1, 1) == 0
 
     def test_4(self):
-        self.assertEqual(distance(-1, -2, -3, -4), 5)
+        assert distance(-1, -2, -3, -4) == 5
 
     def test_5(self):
-        self.assertEqual(distance(1, 1, 1, 2), 1)
+        assert distance(1, 1, 1, 2) == 1
 
     def test_6(self):
-        self.assertEqual(distance(1, 1, 2, 1), 1)
+        assert distance(1, 1, 2, 1) == 1
 
     def test_7(self):
-        self.assertEqual(distance(5, 8, 2, 6), 4)
+        assert distance(5, 8, 2, 6) == 4
 
     def test_8(self):
         distance1 = distance(1, 2, 3, 4)
